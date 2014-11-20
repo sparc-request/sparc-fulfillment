5c944e96bf85d859996fa61a467c22d27fe63e5b
bjk7 <brian.kelsey2@gmail.com>
2014-11-20 10:47:31 -0500

    Add bootstrap-table, removed typeahead, fixed index table to use bootstrap-table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-//= require jquery
 //= require bootstrap-sprockets
 //= require bootstrap-select
+//= require bootstrap-table.min
--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,9 +1,12 @@
 class ProtocolsController < ApplicationController
+  respond_to :json, :html
   def index
     @protocols = Protocol.all
+    respond_with @protocols
   end
 
   def show
     @protocol = Protocol.find(params[:id])
   end
+
 end
