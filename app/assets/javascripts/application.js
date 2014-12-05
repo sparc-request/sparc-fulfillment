e08273f69d7c24eee2d1bc714944bc20c9a15cf4
Stuart Johnson <work.stuart.johnson@gmail.com>
2014-12-05 11:59:35 -0500

    extracing libraries into their own js compiler, to be loaded in <head>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require bootstrap-select
-//= require bootstrap-table.min
 //= require protocols
 //= require participants
 //= require global
