4235e14faef66d2ce547a03cc0d9f0f53156a281
Greg Oleksiak <greg.oleksiak@gmail.com>
2014-12-08 07:48:51 -0500

    Merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141201164925) do
-=======
-ActiveRecord::Schema.define(version: 20141205190108) do
->>>>>>> FETCH_HEAD
 
   create_table "arms", force: true do |t|
     t.integer  "sparc_id"
