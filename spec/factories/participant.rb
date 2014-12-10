fcb2b70bae85cc1e4a746a8a2108a94919fd0332
ctt3 <thiry@musc.edu>
2014-12-10 14:09:29 -0500

    date and phone format validations

---
+++ spec/factories/participant.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+
+  factory :participant do
+    first_name "Honest"
+    last_name "Abe"
+    mrn 5
+    status "Active"
+    date_of_birth "2014-04-12"
+    gender "Male"
+    ethnicity "Not Hispanic or Latino"
+    race "Caucasian"
+    address "1 Lincoln Way"
+    phone "123-456-7890"
+  end
+end
