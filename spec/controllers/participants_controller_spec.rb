50752fab38df1cde108c9c1855be4b87dd97cb25
ctt3 <thiry@musc.edu>
2015-01-06 16:37:42 -0500

    finish participants controller spec

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ParticipantsController do
+RSpec.describe ParticipantsController, type: :controller do
 
   before :each do
     sign_in
@@ -16,14 +16,12 @@ RSpec.describe ParticipantsController do
     expect(assigns(:participants)).to eq([@participant])
   end
 
-  it "should delete a participant" do
-    expect{
-      delete :destroy, {
-        protocol_id: @protocol.id,
-        id: @participant.id,
-        format: :js
-      }
-    }.to change(Participant, :count).by(-1)
+  it "should instantiate a new participant" do
+    xhr :get, :new, {
+      protocol_id: @protocol.id,
+      format: :js
+    }
+    expect(assigns(:participant)).to be_a_new(Participant)
   end
 
   it "should create a new participant" do
@@ -36,6 +34,15 @@ RSpec.describe ParticipantsController do
     }.to change(Participant, :count).by(1)
   end
 
+  it "should select an instantiated participant" do
+    xhr :get, :edit, {
+      protocol_id: @protocol.id,
+      id: @participant.id,
+      format: :js
+    }
+    expect(assigns(:participant)).to eq(@participant)
+  end
+
   it "should update a participant" do
     put :update, {
       protocol_id: @protocol.id,
@@ -45,6 +52,40 @@ RSpec.describe ParticipantsController do
     }
     @participant.reload
     expect(@participant.first_name).to eq 'Chick'
+  end
+
+  it "should delete a participant" do
+    expect{
+      delete :destroy, {
+        protocol_id: @protocol.id,
+        id: @participant.id,
+        format: :js
+      }
+    }.to change(Participant, :count).by(-1)
+  end
+
+  it "should find the participant's arm" do
+    @arm = create(:arm, protocol_id: @protocol.id)
+    @participant.arm = @arm
+    @participant.save
+    xhr :get, :edit_arm, {
+      protocol_id: @protocol.id,
+      participant_id: @participant.id,
+      id: @arm.id,
+      format: :js
+    }
+    expect(assigns(:arm)).to eq(@arm)
+    expect(assigns(:path)).to eq("/protocols/#{@protocol.id}/participants/#{@participant.id}/change_arm/#{@arm.id}")
+  end
+
+  it "should find the participant's arm" do
+    xhr :get, :edit_arm, {
+      protocol_id: @protocol.id,
+      participant_id: @participant.id,
+      format: :js
+    }
+    expect(assigns(:arm)).to be_a_new(Arm)
+    expect(assigns(:path)).to eq("/protocols/#{@protocol.id}/participants/#{@participant.id}/change_arm")
   end
 
   it "should change a participant's arm" do 
