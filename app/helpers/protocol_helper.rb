75ea005d22f940fe1d8aec194af7e3127eda6c0e
Sarah Ford <sarahrandford@gmail.com>
2015-01-06 12:56:29 -0500

    takes outs helper unneccesarily

--- app/helpers/protocol_helper.rb
+++ app/helpers/protocol_helper.rb
@@ -1,12 +1,2 @@
 module ProtocolHelper
-
-  def visit_groups_for_select(arm)
-    visit_group_options = arm.visit_groups
-    visit_group_options.each do |vg|
-      vg.name= "insert before " + vg.name
-    end
-    add_last = VisitGroup.new(name: "add as last", position: visit_group_options.last.position + 1) # creates dummy visit to used as last option, it will not be saved used only for selection
-    visit_group_options << add_last
-    return visit_group_options
-  end
 end
