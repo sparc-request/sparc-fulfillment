cbf57a85424152858efe564a1c557b5d5eeaedb1
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-01-06 15:34:41 -0500

    Participant model specs

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,10 +1,16 @@
 class Participant < ActiveRecord::Base
-  acts_as_paranoid
 
-  after_save :update_via_faye
+  ETHNICITY_OPTIONS = ['Hispanic or Latino', 'Not Hispanic or Latino'].freeze
+  RACE_OPTIONS      = ['Caucasian', 'African American/Black', 'Asian', 'Middle Eastern', 'Pacific Islander', 'Native American/Alaskan', 'Other'].freeze
+  STATUS_OPTIONS    = ['Active', 'Inactive', 'Complete'].freeze
+  GENDER_OPTIONS    = ['Male', 'Female'].freeze
+
+  acts_as_paranoid
 
   belongs_to :protocol
   belongs_to :arm
+
+  after_save :update_via_faye
 
   validates :protocol_id, :first_name, :last_name, :mrn, :date_of_birth, :address, :phone, :status, :ethnicity, :race, :gender, presence: true
   validate :phone_number_format, :date_of_birth_format
@@ -21,26 +27,12 @@ class Participant < ActiveRecord::Base
     end
   end
 
+  private
+
   def update_via_faye
     channel = "/participants/#{self.protocol.id}/list"
-    message = {:channel => channel, :data => "woohoo", :ext => {:auth_token => ENV['FAYE_TOKEN']}}
-    uri = URI.parse("http://localhost:9292/faye")
-    Net::HTTP.post_form(uri, :message => message.to_json)
-  end
-
-  def self.ethnicity_options
-    ['Hispanic or Latino', 'Not Hispanic or Latino']
-  end
-
-  def self.race_options
-    ['Caucasian', 'African American/Black', 'Asian', 'Middle Eastern', 'Pacific Islander', 'Native American/Alaskan', 'Other']
-  end
-
-  def self.status_options
-    ['Active', 'Inactive', 'Complete']
-  end
-
-  def self.gender_options
-    ['Male', 'Female']
+    message = { channel: channel, data: "woohoo", ext: { auth_token: ENV.fetch('FAYE_TOKEN') } }
+    uri = URI.parse('http://' + ENV.fetch('CWF_FAYE_HOST') + '/faye')
+    Net::HTTP.post_form(uri, message: message.to_json)
   end
 end
