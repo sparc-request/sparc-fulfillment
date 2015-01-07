0c249b5a0f2b553dd635de3dfb5f84ff13df9491
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-01-07 08:38:07 -0500

    Add Arm model specs

--- app/models/arm.rb
+++ app/models/arm.rb
@@ -1,10 +1,11 @@
 class Arm < ActiveRecord::Base
+
   acts_as_paranoid
 
   belongs_to :protocol
 
-  has_many :line_items, :dependent => :destroy
-  has_many :visit_groups, :dependent => :destroy
+  has_many :line_items, dependent: :destroy
+  has_many :visit_groups, dependent: :destroy
   has_many :participants
 
   accepts_nested_attributes_for :line_items, :visit_groups
@@ -16,12 +17,14 @@ class Arm < ActiveRecord::Base
   after_create :create_visit_groups
 
   private
+
   def create_visit_groups
     new_visit_group_columns = [:name, :day, :position, :arm_id]
-    new_visit_group_values = []
+    new_visit_group_values  = []
+
     for count in 1..self.visit_count
       new_visit_group_values << ["Visit "+ count.to_s, count, count, self.id]
     end
-    VisitGroup.import new_visit_group_columns, new_visit_group_values, {validate: true}
+    VisitGroup.import new_visit_group_columns, new_visit_group_values, { validate: true }
   end
 end
--- spec/models/arm_spec.rb
+++ spec/models/arm_spec.rb
@@ -4,6 +4,50 @@ RSpec.describe Arm, type: :model do
 
   it { should belong_to(:protocol) }
 
-  it { should have_many(:line_items) }
-  it { should have_many(:visit_groups) }
+  it { should have_many(:line_items).dependent(:destroy) }
+  it { should have_many(:visit_groups).dependent(:destroy) }
+  it { should have_many(:participants) }
+
+  it { should accept_nested_attributes_for(:line_items) }
+  it { should accept_nested_attributes_for(:visit_groups) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_numericality_of(:subject_count).is_greater_than_or_equal_to(1) }
+  it { should validate_numericality_of(:visit_count).is_greater_than_or_equal_to(1) }
+
+  context 'class methods' do
+
+    describe '#delete' do
+
+      it 'should not permanently delete the record' do
+        arm = create(:arm)
+
+        arm.delete
+
+        expect(arm.persisted?).to be
+      end
+    end
+
+    describe 'callbacks' do
+
+      describe '#create_visit_groups' do
+
+        it 'should callback #create_visit_groups after #create' do
+          arm = create(:arm)
+
+          expect(arm).to callback(:create_visit_groups).after(:create)
+        end
+
+        it 'should import VisitGroups' do
+          arm = create(:arm, visit_count: 2)
+
+          expect(arm.visit_groups.count).to eq(2)
+        end
+      end
+    end
+  end
+
+  context 'instance methods' do
+
+  end
 end
