c27d71a606853b063cfdcc4f4ca010f7b2202dff
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-01-14 11:10:16 -0500

    User views protocol
    Current IRB Expiration Date is correctly formatted

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -3,4 +3,5 @@ Dir[Rails.root.join("spec/support/features/*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.include Features::BootstrapHelpers, type: :feature
   config.include Features::DataHelpers, type: :feature
+  config.include Features::ProtocolHelpers, type: :feature
 end
