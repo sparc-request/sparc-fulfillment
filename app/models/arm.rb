13e706f99b6d677767570f795b98190d38e29749
ctt3 <thiry@musc.edu>
2015-01-14 10:20:59 -0500

    merge conflicts 2

--- app/models/arm.rb
+++ app/models/arm.rb
@@ -12,21 +12,7 @@ class Arm < ActiveRecord::Base
   validates_numericality_of :subject_count, greater_than_or_equal_to: 1
   validates_numericality_of :visit_count, greater_than_or_equal_to: 1
 
-  after_create :create_visit_groups
-
   def line_items_grouped_by_core
     line_items.includes(:service).group_by(&:sparc_core_id)
-  end
-
-  private
-
-  def create_visit_groups
-    new_visit_group_columns = [:name, :day, :position, :arm_id]
-    new_visit_group_values  = []
-
-    for count in 1..self.visit_count
-      new_visit_group_values << ["Visit "+ count.to_s, count, count, self.id]
-    end
-    VisitGroup.import new_visit_group_columns, new_visit_group_values, { validate: true }
   end
 end
