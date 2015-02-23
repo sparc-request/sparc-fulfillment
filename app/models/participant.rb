c5499f388e8c2a17642152ea530462beb300da9c
Andrew Cates <amcates@gmail.com>
2015-02-23 09:27:34 -0500

    adding after_destroy

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -12,6 +12,7 @@ class Participant < ActiveRecord::Base
   has_many :appointments
 
   after_save :update_faye
+  after_destroy :update_faye
 
   validates :protocol_id, :first_name, :last_name, :mrn, :date_of_birth, :phone, :ethnicity, :race, :gender, presence: true
   validate :phone_number_format, :date_of_birth_format
