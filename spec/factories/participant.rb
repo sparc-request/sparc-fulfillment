748615ed3dc5a622988af40c07295559736d410c
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-02-24 11:47:50 -0500

    Refactor Participants List specs

--- spec/factories/participant.rb
+++ spec/factories/participant.rb
@@ -1,16 +1,5 @@
 FactoryGirl.define do
 
-#    first_name 'First name'
-#    last_name 'Last name'
-#    mrn 5
-#    status 'Active'
-#    date_of_birth Time.current.last_year
-#    gender 'Male'
-#    ethnicity 'Not Hispanic or Latino'
-#    race 'Caucasian'
-#    address 'Address'
-#    phone '123-456-7890'
-
   factory :participant do
     arm nil
     protocol nil
