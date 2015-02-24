7f57a854a620637fa1fd0b62e4dc22c00d0d6f35
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-02-24 14:04:00 -0500

    Fix user_deletes_participant_apec

--- spec/features/participants/user_deletes_participant_spec.rb
+++ spec/features/participants/user_deletes_participant_spec.rb
@@ -5,7 +5,7 @@ feature 'User deletes Participant', js: true do
   scenario 'and sees the Participant is remove from the list' do
     protocol = create(:protocol_imported_from_sparc)
 
-    visit protocol_path(protocol)
+    visit protocol_path(protocol.sparc_id)
     click_link 'Participant List'
     accept_confirm do
       page.find('table.participants tbody tr:first-child td.delete a').click
