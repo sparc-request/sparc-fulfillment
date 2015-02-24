9e83d7d9f3227bdaad639e74166659f29ffc4c03
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-02-24 12:22:05 -0500

    Refactor Participant Tracker specs

--- spec/features/participants/user_views_participant_tracker_spec.rb
+++ spec/features/participants/user_views_participant_tracker_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
-feature 'User views Participant list', js: true do
+feature 'User views Participant Tracker', js: true do
 
-  before do
+  before :each do
     protocol = create(:protocol_imported_from_sparc)
 
     visit protocol_path(protocol.sparc_id)
-    click_link 'Participant List'
+    click_link 'Participant Tracker'
   end
 
   scenario 'and sees Participants' do
-    expect(page).to have_css('table.participants tbody td', count: 3)
+    participant_first_names = Participant.all.map(&:first_name)
+
+    participant_first_names.each do |first_name|
+      expect(page).to have_css('table.participants tbody td.first_name', first_name)
+    end
   end
 
   scenario 'and searches for an existing Participant' do
@@ -19,11 +23,5 @@ feature 'User views Participant list', js: true do
     search_bootstrap_table participant.first_name
 
     expect(page).to have_css('table.participants tbody tr:first-child td.first_name', text: participant.first_name)
-  end
-
-  scenario 'and searches for an non-existing Participant' do
-    search_bootstrap_table 'Superman'
-
-    expect(page).to have_css('table.participants tbody', text: 'No matching records found')
   end
 end
