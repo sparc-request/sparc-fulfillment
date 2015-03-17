e8a7b7e71130c1f8985ea09abd188051504689cc
Jason Leonard <jleonardw9@gmail.com>
2015-03-17 09:56:10 -0400

    Adding back in validations.

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,5 +6,5 @@ class Task < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :assignee, class_name: "User"
-  validates :due_date, presence: true
+  validates :assignee_id, :due_date, presence: true
 end
