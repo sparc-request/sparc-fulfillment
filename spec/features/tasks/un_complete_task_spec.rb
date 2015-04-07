f4807f582fa2e04fac87555ddf0af7db07f5b0b4
Jason Leonard <jleonardw9@gmail.com>
2015-04-07 11:16:37 -0400

    Fixing uncomplete task spec.

--- spec/features/tasks/un_complete_task_spec.rb
+++ spec/features/tasks/un_complete_task_spec.rb
@@ -15,15 +15,17 @@ feature "un-completing a Task", js: true do
 
     visit tasks_path
     first('input.complete').click
+    wait_for_ajax
+    expect(page).to have_css("table.tasks tbody tr", count: 1)
   end
 
   def when_i_set_the_task_to_incomplete
-    find("button.complete").click
+    find("#complete").click
     wait_for_ajax
     first('input.complete').click
   end
 
   def then_i_should_see_that_the_task_is_incomplete
-    expect(page).to_not have_css("table.tasks tbody tr")
+    expect(page).to have_css("table.tasks tbody tr", count: 2)
   end
 end
