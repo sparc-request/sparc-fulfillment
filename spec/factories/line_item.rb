6f28b0290f976f28bf84a6c75f590c73f46eb4b4
ctt3 <thiry@musc.edu>
2015-04-13 13:35:22 -0400

    associate protocol to line item, otfs have no arm

--- spec/factories/line_item.rb
+++ spec/factories/line_item.rb
@@ -3,6 +3,16 @@ FactoryGirl.define do
   factory :line_item do
     arm nil
     service nil
+    protocol nil
     sparc_id
+    otf false
+
+    trait :otf do
+      otf true
+      quantity { Faker::Number.number(3) }
+      cost { Faker::Commerce.price }
+    end
+
+    factory :line_item_otf, traits:[:otf]
   end
 end
