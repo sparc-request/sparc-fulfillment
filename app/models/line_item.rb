5ba286ac8c8cc5459f771c18aeb9d70f9e856bd3
ctt3 <thiry@musc.edu>
2015-04-15 12:56:20 -0400

    SLC begin

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -8,10 +8,34 @@ class LineItem < ActiveRecord::Base
 
   has_many :visit_groups, through: :arm
   has_many :visits, -> { includes(:visit_group).order("visit_groups.position") }, dependent: :destroy
+  has_many :fulfillments
 
   delegate  :name,
             :cost,
             :sparc_core_id,
             :sparc_core_name,
             to: :service
+
+  def quantity_remaining
+    if otf and fulfillments
+      remaining = quantity
+      fulfillments.each do |f|
+        remaining -= f.quantity
+      end
+
+      remaining
+    end
+  end
+
+  def date_started
+    if otf and !fulfillments.empty?
+      fulfillments.dato.first.created_at
+    end
+  end
+
+  def last_fulfillment
+    if otf and !fulfillments.empty?
+      fulfillments.dato.last.updated_at
+    end
+  end
 end
