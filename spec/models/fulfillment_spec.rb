c35636b885ea8beffb1e588a0505ff1058669119
ctt3 <thiry@musc.edu>
2015-05-04 11:17:39 -0400

    fulfillment model spec

--- spec/models/fulfillment_spec.rb
+++ spec/models/fulfillment_spec.rb
@@ -4,8 +4,21 @@ RSpec.describe Fulfillment, type: :model do
 
   it { is_expected.to belong_to(:line_item) }
 
-  it { is_expected.to have_many(:components) }
+  it { is_expected.to have_one(:component) }
   it { is_expected.to have_many(:notes) }
   it { is_expected.to have_many(:documents) }
+
+  context 'instance methods' do
+
+    describe '.quantity_type' do
+
+      it 'should be delegated to LineItem' do
+        line_item = create(:line_item, quantity_type: 'Each')
+        fulfillment = create(:fulfillment, line_item: line_item)
+
+        expect(fulfillment.quantity_type).to eq(line_item.quantity_type)
+      end
+    end
+  end
 
 end 
