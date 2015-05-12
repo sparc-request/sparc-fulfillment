28b048bb6881d6645eaf294ff997269a8cd9b619
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-05-12 15:42:14 -0400

    Fix Service.all_cached

--- app/models/service.rb
+++ app/models/service.rb
@@ -10,6 +10,12 @@ class Service < ActiveRecord::Base
 
   default_scope { order(name: :asc) }
 
+  def self.all_cached
+    Rails.cache.fetch("services_all", expires_in: 1.hour) do
+      Service.all
+    end
+  end
+
   # TODO Determine exact cost calculation
   def cost
     if pricing_map = pricing_maps.current(Time.current).first
