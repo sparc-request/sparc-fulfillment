c2c3079e473e723a56a1126cea1119b61f4bf9d0
Charlie Thiry <thiry@musc.edu>
2015-05-12 15:04:29 -0400

    repair model specs

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe LineItem, type: :model do
 
       it 'should be delegated to Service' do
         service   = create(:service, name: 'Test Service')
-        line_item = create(:line_item, service: service)
+        line_item = create(:line_item, service: service, protocol: create(:protocol))
 
         expect(line_item.name).to eq('Test Service')
       end
@@ -29,7 +29,7 @@ RSpec.describe LineItem, type: :model do
 
       it 'should be delegated to Service' do
         service   = create(:service, cost: 1)
-        line_item = create(:line_item, service: service)
+        line_item = create(:line_item, service: service, protocol: create(:protocol))
 
         expect(line_item.cost).to eq(1)
       end
@@ -39,7 +39,7 @@ RSpec.describe LineItem, type: :model do
 
       it 'should be delegated to Service' do
         service   = create(:service, sparc_core_name: 'Core A')
-        line_item = create(:line_item, service: service)
+        line_item = create(:line_item, service: service, protocol: create(:protocol))
 
         expect(line_item.sparc_core_name).to eq('Core A')
       end
@@ -49,7 +49,7 @@ RSpec.describe LineItem, type: :model do
 
       it 'should be delegated to Service' do
         service   = create(:service, sparc_core_id: 4)
-        line_item = create(:line_item, service: service)
+        line_item = create(:line_item, service: service, protocol: create(:protocol))
 
         expect(line_item.sparc_core_id).to eq(4)
       end
@@ -59,7 +59,7 @@ RSpec.describe LineItem, type: :model do
 
       it 'should be delegated to Service' do
         service   = create(:service, one_time_fee: true)
-        line_item = create(:line_item, service: service)
+        line_item = create(:line_item, service: service, protocol: create(:protocol))
 
         expect(line_item.one_time_fee).to eq(true)
       end
@@ -72,7 +72,7 @@ RSpec.describe LineItem, type: :model do
 
       it 'should return the quantity_requested minus the quantity of each fulfillment' do
         service   = create(:service, one_time_fee: true)
-        line_item = create(:line_item, service: service, quantity_requested: 500)
+        line_item = create(:line_item, service: service, protocol: create(:protocol), quantity_requested: 500)
         4.times do
           create(:fulfillment, line_item: line_item, quantity: 25)
         end
@@ -82,7 +82,7 @@ RSpec.describe LineItem, type: :model do
 
       it 'should return quantity_requested if there are no fulfillments' do
         service   = create(:service, one_time_fee: true)
-        line_item = create(:line_item, service: service, quantity_requested: 500)
+        line_item = create(:line_item, service: service, protocol: create(:protocol), quantity_requested: 500)
 
         expect(line_item.quantity_remaining).to eq(500)
       end
@@ -92,10 +92,10 @@ RSpec.describe LineItem, type: :model do
 
       it 'should return the fulfilled_at date of the latest fulfillment' do
         service   = create(:service, one_time_fee: true)
-        line_item = create(:line_item, service: service)
-        fill_1 = create(:fulfillment, line_item: line_item, fulfilled_at: Date.today - 2.weeks)
-        fill_2 = create(:fulfillment, line_item: line_item, fulfilled_at: Date.today - 1.week)
-        fill_3 = create(:fulfillment, line_item: line_item, fulfilled_at: Date.today - 1.day)
+        line_item = create(:line_item, service: service, protocol: create(:protocol))
+        fill_1 = create(:fulfillment, line_item: line_item, fulfilled_at: "08-10-2015")
+        fill_2 = create(:fulfillment, line_item: line_item, fulfilled_at: "09-10-2015")
+        fill_3 = create(:fulfillment, line_item: line_item, fulfilled_at: "10-10-2015")
 
         expect(line_item.last_fulfillment).to eq(fill_3.fulfilled_at)
       end
