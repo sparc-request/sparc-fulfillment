08e7203f3710e679a6adc81085f2da277774194f
Charlie Thiry <thiry@musc.edu>
2015-05-14 14:50:56 -0400

    reorganizing the yml and fixing specs

--- spec/features/protocols/study_schedule_edit_buttons_spec.rb
+++ spec/features/protocols/study_schedule_edit_buttons_spec.rb
@@ -139,7 +139,7 @@ RSpec.describe 'Study Schedule Edit Buttons spec', type: :feature, js: true do
 
       it "should remove the visit group from the service calendar" do
         vg = arm1.visit_groups.first
-        bootstrap_select "#visits", "#{vg.id}"
+        bootstrap_select "#visits", "#{vg.name}"
         wait_for_ajax
         find('#remove_visit_button').click()
         page.driver.browser.accept_js_confirms
