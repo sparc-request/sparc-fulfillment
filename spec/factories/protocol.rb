0d9c266288349e97ecd4d61940840d97ccb5d5f0
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-05-21 16:32:33 -0400

    Update protocol.rb

--- spec/factories/protocol.rb
+++ spec/factories/protocol.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
 
   factory :protocol, aliases: [:protocol_complete] do
     sparc_id
+    sparc_sub_service_request_id 1
     title { Faker::Company.catch_phrase }
     short_title { Faker::Company.catch_phrase }
     sponsor_name { Faker::Company.name }
