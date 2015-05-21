c2c26f494fcdb523979f9619f7f94c69100f2f14
Jason Leonard <jleonardw9@gmail.com>
2015-05-21 11:27:50 -0400

    Formated the appointment name to account for there not being a visit group. [#95004206]

--- app/helpers/appointment_helper.rb
+++ app/helpers/appointment_helper.rb
@@ -10,4 +10,12 @@ module AppointmentHelper
 
     old_statuses
   end
+
+  def format_name appointment
+    if appointment.visit_group
+      return appointment.visit_group.name
+    else
+      return appointment.name
+    end
+  end
 end
--- app/views/appointments/completed_appointments.json.jbuilder
+++ app/views/appointments/completed_appointments.json.jbuilder
@@ -1,4 +1,4 @@
 json.(@appointments) do |appointment|
-  json.name appointment.visit_group.name
+  json.name format_name(appointment)
   json.completed_date appointment.completed_date.strftime('%x')
 end
