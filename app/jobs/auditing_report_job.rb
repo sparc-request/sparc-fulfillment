941bcd61c92f6c14cb223336a783308052406b10
Stuart Johnson <work.stuart.johnson@gmail.com>
2015-05-26 11:11:36 -0400

    removing report lines temporarily

--- app/jobs/auditing_report_job.rb
+++ app/jobs/auditing_report_job.rb
@@ -15,17 +15,17 @@ class AuditingReportJob < ActiveJob::Base
         protocols = Protocol.all
       end
 
-      protocols.each do |protocol|
-        protocol.procedures.to_a.select{|procedure| procedure.appointment.completed_date && (start_date..end_date).cover?(procedure.appointment.completed_date)}.group_by(&:service).each do |group, procedures|
-          r_count = procedures.count{|p| p.billing_type == "research_billing_qty"}
-          t_count = procedures.count{|p| p.billing_type == "insurance_billing_qty"}
+      # protocols.each do |protocol|
+      #   protocol.procedures.to_a.select{|procedure| procedure.appointment.completed_date && (start_date..end_date).cover?(procedure.appointment.completed_date)}.group_by(&:service).each do |group, procedures|
+      #     r_count = procedures.count{|p| p.billing_type == "research_billing_qty"}
+      #     t_count = procedures.count{|p| p.billing_type == "insurance_billing_qty"}
 
-          procedure = procedures.first
-          participant = procedure.participant
-          appointment = procedure.appointment
-          csv << [protocol.sparc_id, participant.full_name, participant.label, appointment.arm.name, appointment.name, appointment.completed_date, procedure.service.organization.name, procedure.service_name, "R Due", "R Completed", "Why Not?", "Total Cost", "T Due", "T Completed"]
-        end
-      end
+      #     procedure = procedures.first
+      #     participant = procedure.participant
+      #     appointment = procedure.appointment
+      #     csv << [protocol.sparc_id, participant.full_name, participant.label, appointment.arm.name, appointment.name, appointment.completed_date, procedure.service.organization.name, procedure.service_name, "R Due", "R Completed", "Why Not?", "Total Cost", "T Due", "T Completed"]
+      #   end
+      # end
     end
 
     report = Report.find(report_id)
@@ -37,10 +37,3 @@ class AuditingReportJob < ActiveJob::Base
     FayeJob.enqueue(report)
   end
 end
-
-##########Notes##########
-#Is "Visit Date" the completed date of the appointment?
-#For the start and end date, am I checking the beginning, or the end of the appointment?
-#Does total cost include the "T" stuff?
-#What do I do with "O" billing type? Just ignore it?
-#Why not column doesn't make sense. Each of these entries represents multiple procedures, which could each have a "reason."
