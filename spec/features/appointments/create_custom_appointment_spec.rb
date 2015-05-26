2d1224fd1080308dd8f61f6bab7bc1bd45a2ca2f
Jason Leonard <jleonardw9@gmail.com>
2015-05-26 13:46:44 -0400

    Adding specs.

--- spec/features/appointments/create_custom_appointment_spec.rb
+++ spec/features/appointments/create_custom_appointment_spec.rb
@@ -11,6 +11,10 @@ feature 'Custom appointment', js: true do
     when_i_click_save_appointment
     wait_for_ajax
     then_i_should_see_the_newly_created_appointment
+    when_i_select_the_appointment
+    then_add_a_procedure
+    then_i_complete_it
+    then_it_should_appear_on_the_dashboard
   end
 
   def when_i_visit_a_participants_calendar
@@ -38,5 +42,32 @@ feature 'Custom appointment', js: true do
 
   def then_i_should_see_the_newly_created_appointment
     expect(page).to have_css("#appointment_select option", visible: false, text: "Test Visit")
+  end
+
+  def when_i_select_the_appointment
+    @service = Service.per_participant_visits.first
+    @service.update_attributes(name: 'Test Service')
+    bootstrap_select '#appointment_select', "Test Visit"
+    wait_for_ajax
+  end
+
+  def then_add_a_procedure
+    screenshot
+    bootstrap_select '#service_list', 'Test Service'
+    fill_in 'service_quantity', with: 1
+    find('button.add_service').click
+    wait_for_ajax
+  end
+
+  def then_i_complete_it
+    find('button.start_visit').click
+    find('label.status.complete').click
+    wait_for_ajax
+    click_button 'Complete Visit'
+  end
+
+  def then_it_should_appear_on_the_dashboard
+    screenshot
+    expect(page).to have_content('Test Visit')
   end
 end
