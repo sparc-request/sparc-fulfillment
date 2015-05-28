1dbba2861801dc93035c3225ffdd32aec5515e9a
Charlie Thiry <thiry@musc.edu>
2015-05-28 16:50:33 -0400

    remove unnecessaries

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( libraries.js )
-Rails.application.config.assets.precompile += %w( select_appointment.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
