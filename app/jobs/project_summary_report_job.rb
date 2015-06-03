8631b29b9a4f55020c02115d4cae2dc212e19a67
Stuart Johnson <work.stuart.johnson@gmail.com>
2015-06-03 15:49:17 -0400

    project summary report

--- app/jobs/project_summary_report_job.rb
+++ app/jobs/project_summary_report_job.rb
@@ -7,16 +7,14 @@ class ProjectSummaryReportJob < ActiveJob::Base
   def perform(report_id, start_date, end_date, protocol_id)
     CSV.open("tmp/project_summary_report.csv", "wb") do |csv|
       protocol = Protocol.find(protocol_id)
-      csv << ["SPARC ID: #{protocol.sparc_id}"]
-      csv << ["PI Name: #{protocol.pi ? protocol.pi.full_name : nil}"]
+      csv << ["SPARC ID:", "#{protocol.sparc_id}", "", "Appointment Start Date Filter:", "#{format_date(start_date.to_date)}"]
+      csv << ["PI Name:", "#{protocol.pi ? protocol.pi.full_name : nil}", "", "Appointment End Date Filter:", "#{format_date(end_date.to_date)}"]
       csv << ["?"]
-      csv << ["Routing: "]
-      csv << ["Appointment Start Date Filter: #{format_date(start_date.to_date)}"]
-      csv << ["Appointment End Date Filter: #{format_date(end_date.to_date)}"]
+      csv << ["Routing:", ""]
       csv << [""]
 
       # amount due for whole study
-      amount_due = 0
+      arms_total = 0
 
       protocol.arms.each do |arm|
         visit_groups          = arm.visit_groups
@@ -25,8 +23,12 @@ class ProjectSummaryReportJob < ActiveJob::Base
         participant_totals    = []  # totals per participant
 
         csv << [""]
-        csv << ["Arm: #{arm.name}"]
-        csv << ["Participant ID", "Status", visit_groups.pluck(:name), "Totals"].flatten
+        csv << [""]
+        csv << [""]
+        csv << [""]
+        csv << ["Arm Name: #{arm.name}"]
+        csv << ["", "Participant ID", "Participant Status", visit_groups.pluck(:name), "Totals"].flatten
+        csv << [""]
 
         participants.each do |participant|
           participant_costs = visit_groups.map do |vg|
@@ -37,30 +39,38 @@ class ProjectSummaryReportJob < ActiveJob::Base
           participant_totals    << participant_total
           visit_group_subtotals = add_parallel_arrays(visit_group_subtotals, participant_costs)
 
-          csv << ["Subject #{participant.label}", participant.status, display_cost_array(participant_costs + [participant_total])].flatten
+          csv << ["", "Subject #{participant.label}", participant.status, display_cost_array(participant_costs + [participant_total])].flatten
         end
 
-        arm_total = sum_up(visit_group_subtotals)
+        arm_subtotal = sum_up(visit_group_subtotals)
         csv << [""]
-        csv << ["Visit Subtotals - #{arm.name}", "", display_cost_array(visit_group_subtotals + [arm_total])].flatten
-        amount_due += arm_total
+        csv << ["", "Visit Subtotals - #{arm.name}", "", display_cost_array(visit_group_subtotals + [arm_subtotal])].flatten
+        arms_total += arm_subtotal
       end
 
       csv << [""]
-      csv << ["One Time Fees"]
-      csv << ["Name", "Cost"]
+      csv << [""]
+      csv << [""]
+      csv << [""]
+      csv << [""]
+      csv << [""]
+      csv << ["Study Level Charges"]
+      csv << ["", "Name", "Cost"]
+      csv << [""]
 
-      one_time_fees = 0
+      study_level_charges = 0
       protocol.fulfillments.fulfilled_in_date_range(start_date, end_date).each do |f|
-        csv << [f.service_name, format_cost(f.service_cost)]
-        one_time_fees += f.service_cost
+        csv << ["", f.service_name, display_cost(f.service_cost)]
+        study_level_charges += f.service_cost
       end
 
-      csv << ["Total", "", display_cost(one_time_fees)]
       csv << [""]
-      csv << ["Study Totals", display_cost(amount_due += one_time_fees)]
       csv << [""]
-      csv << ["Amount Due", display_cost(amount_due)]
+      csv << ["Study Level Charges Total", display_cost(study_level_charges)]
+      csv << ["Arms Total", display_cost(arms_total)]
+      csv << [""]
+      csv << [""]
+      csv << ["Study Total", display_cost(arms_total + study_level_charges)]
     end
 
     report = Report.find(report_id)
