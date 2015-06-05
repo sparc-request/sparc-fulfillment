bf7c20b817861016880bcc20829eb50e3c3e7c58
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-06-05 08:30:04 -0400

    Organization.inclusive_child_services should only return Services of Organizations which are not process_ssrs

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,8 +6,10 @@ class Organization < ActiveRecord::Base
 
   has_many :services
   has_many :sub_service_requests
-  has_many :children, class_name: "Organization",
-                      foreign_key: :parent_id
+  has_many :non_process_ssrs_children,
+            -> { where(process_ssrs: false) },
+            class_name: "Organization",
+            foreign_key: :parent_id
 
   def inclusive_child_services(scope)
     services.
@@ -17,18 +19,21 @@ class Organization < ActiveRecord::Base
       sort_by(&:name)
   end
 
-  def protocols
-    Protocol.
-      joins(:sub_service_request).
-      where(sub_service_requests: { organization_id: id })
-  end
-
   def all_child_organizations
-    [children, children.map(&:all_child_organizations)].flatten
+    [
+      non_process_ssrs_children,
+      non_process_ssrs_children.map(&:all_child_organizations)
+    ].flatten
   end
 
   def all_child_services(scope)
     all_child_organizations.map { |child| child.services.send(scope) }
+  end
+
+  def protocols
+    Protocol.
+      joins(:sub_service_request).
+      where(sub_service_requests: { organization_id: id })
   end
 end
 
