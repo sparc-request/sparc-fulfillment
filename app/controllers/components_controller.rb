199e161552873004079bb66677a5093c7fffd4e7
Charlie Thiry <thiry@musc.edu>
2015-06-17 10:28:40 -0400

    line item components now tracked with notes

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,15 +1,26 @@
 class ComponentsController < ApplicationController
 
   def update
-    if params[:components] == ""
-      component_ids_to_select = []
-    else
-      component_ids_to_select = params[:components].reject(&:empty?).map(&:to_i)
+    line_item = LineItem.find(params[:line_item_id])
+    new_component_ids = params[:components] == "" ? [] : params[:components].reject(&:empty?).map(&:to_i)
+    old_component_ids = line_item.components.where(selected: true).map(&:id)
+
+    to_select = new_component_ids - old_component_ids
+    to_select.each do |id|
+      component = Component.find(id)
+      component.update_attributes(selected: true)
+      comment = "Component: #{component.component} indicated"
+      line_item.notes.create(kind: 'log', comment: comment, identity: current_identity)
     end
 
-    line_item_components = Component.where(composable_id: params[:line_item_id], composable_type: "LineItem")
-    line_item_components.update_all(selected: false)
-    component_ids_to_select.each{ |id| line_item_components.find(id).update(selected: true)}
+    to_deselect = old_component_ids - new_component_ids
+    to_deselect.each do |id|
+      component = Component.find(id)
+      component.update_attributes(selected: false)
+      comment = "Component: #{component.component} no longer indicated"
+      line_item.notes.create(kind: 'log', comment: comment, identity: current_identity)
+    end
+
     flash[:success] = t(:line_item)[:flash_messages][:updated]
   end
 end
