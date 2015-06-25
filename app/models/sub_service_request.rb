5994f5555e24b2c75648440e9654910123f070fd
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-06-25 09:37:28 -0400

    Refactor SubServiceRequest statuses to localizations
    Refactor Protocol.owner and Protocol.requester
    
    Adding addition attributes for protocol and delegating status to sub service requests. [#97402392]
    
    Adding additional attributes to protocol index page. [#97402392]
    
    Making fixes to spec files.
    
    Forgot to migrate
    
    Factor out unneeded method

--- app/models/sub_service_request.rb
+++ app/models/sub_service_request.rb
@@ -1,9 +1,32 @@
 class SubServiceRequest < ActiveRecord::Base
 
+  STATUSES = [
+    'all',
+    'draft',
+    'submitted',
+    'get_a_quote',
+    'in_process',
+    'complete',
+    'awaiting_pi_approval',
+    'on_hold',
+    'ctrc_review',
+    'ctrc_approved',
+    'administrative_review',
+    'committee_review',
+    'invoiced',
+    'fulfillment_queue',
+    'approved',
+    'declined',
+    'withdrawn'
+  ].freeze
+
   include SparcShard
 
   has_one :protocol
 
+  belongs_to :owner, class_name: 'Identity'
   belongs_to :organization
   belongs_to :service_request
+
+  delegate :requester, to: :service_request
 end
--- db/schema.rb
+++ db/schema.rb
@@ -224,8 +224,8 @@ ActiveRecord::Schema.define(version: 20150624134812) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "visit_id",         limit: 4
-    t.integer  "performer_id",     limit: 4
     t.datetime "incompleted_date"
+    t.integer  "performer_id",     limit: 4
   end
 
   add_index "procedures", ["appointment_id"], name: "index_procedures_on_appointment_id", using: :btree
@@ -249,7 +249,6 @@ ActiveRecord::Schema.define(version: 20150624134812) do
     t.float    "stored_percent_subsidy", limit: 24
     t.integer  "study_cost",             limit: 4
     t.integer  "sub_service_request_id", limit: 4
-    t.string   "status",                 limit: 255
   end
 
   add_index "protocols", ["deleted_at"], name: "index_protocols_on_deleted_at", using: :btree
@@ -272,6 +271,42 @@ ActiveRecord::Schema.define(version: 20150624134812) do
   add_index "tasks", ["assignable_id", "assignable_type"], name: "index_tasks_on_assignable_id_and_assignable_type", using: :btree
   add_index "tasks", ["assignee_id"], name: "index_tasks_on_assignee_id", using: :btree
   add_index "tasks", ["identity_id"], name: "index_tasks_on_identity_id", using: :btree
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id",     limit: 4
+    t.integer  "protocol_id", limit: 4
+    t.string   "rights",      limit: 255
+    t.string   "role",        limit: 255
+    t.string   "role_other",  limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "deleted_at"
+  end
+
+  add_index "user_roles", ["protocol_id"], name: "index_user_roles_on_protocol_id", using: :btree
+  add_index "user_roles", ["user_id"], name: "index_user_roles_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",                           null: false
+    t.string   "encrypted_password",     limit: 255, default: "",                           null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,                            null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.string   "time_zone",              limit: 255, default: "Eastern Time (US & Canada)"
+    t.integer  "tasks_count",            limit: 4,   default: 0
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
     t.string   "item_type",  limit: 255,   null: false
