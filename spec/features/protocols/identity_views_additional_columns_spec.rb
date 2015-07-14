6edfd8a88539faf45cf5e8efd543ea408c1bd33f
Jason Leonard <jleonardw9@gmail.com>
2015-07-14 11:03:53 -0400

    Fixing the data helper to account for grandparent organizations.

--- spec/features/protocols/identity_views_additional_columns_spec.rb
+++ spec/features/protocols/identity_views_additional_columns_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 feature 'Identity views additional columns', js: true do
-  let!(:identity)            { Identity.first }
-  let!(:provider)            { create(:organization_provider, name: "Test Provider") }
-  let!(:program)             { create(:organization_program, parent: provider, name: "Test Program") }
-  let!(:core)                { create(:organization_core, parent: program, name: "Test Core") }
-  let!(:sub_service_request) { create(:sub_service_request, organization: core) }
-  let!(:protocol)            { create(:protocol, sub_service_request: sub_service_request) }
-  let!(:clinical_provider)   { create(:clinical_provider, identity: identity, organization: core) }
-  let!(:pi)                  { create(:project_role_pi, identity: identity, protocol: protocol) }
+
+  before :each do
+    create_and_assign_protocol_to_me
+  end
 
   scenario 'user adds the provider column' do
     as_a_user_who_visits_the_protocols_index_page
@@ -41,7 +37,7 @@ feature 'Identity views additional columns', js: true do
 
   def i_should_see_the_provider_column
     wait_for_ajax
-    expect(page).to have_content('Test Provider')
+    expect(page).to have_content('Provider')
   end
 
   def when_i_select_program_from_the_dropdown
@@ -52,7 +48,7 @@ feature 'Identity views additional columns', js: true do
 
   def i_should_see_the_program_column
     wait_for_ajax
-    expect(page).to have_content('Test Program')
+    expect(page).to have_content('Program')
   end
 
   def when_i_select_core_from_the_dropdown
@@ -63,6 +59,6 @@ feature 'Identity views additional columns', js: true do
 
   def i_should_see_the_core_column
     wait_for_ajax
-    expect(page).to have_content('Test Core')
+    expect(page).to have_content('Core')
   end
 end
