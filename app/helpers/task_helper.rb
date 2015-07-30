9ca6ce18d70325c954c5081ed19b1d3c6dc27d0b
Kyle Glick <kyleglick99@gmail.com>
2015-07-30 10:22:52 -0400

    replaced # href with javascript:void(0) on tasks page

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -3,7 +3,7 @@ module TaskHelper
   def format_reschedule(task_id)
     html = '-'
     icon = raw content_tag(:i, '', class: "glyphicon glyphicon-calendar")
-    html = raw content_tag(:a, raw(icon), class: 'task-reschedule', href: '#', task_id: task_id)
+    html = raw content_tag(:a, raw(icon), class: 'task-reschedule', href: 'javascript:void(0)', task_id: task_id)
 
     html
   end
