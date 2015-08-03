9dfaa6a694833bae83b4517fa5dba1fc531df50f
Kyle Glick <kyleglick99@gmail.com>
2015-08-03 15:16:56 -0400

    Participant specs refactor

--- spec/features/participants/participant_list/user_creates_participant_spec.rb
+++ spec/features/participants/participant_list/user_creates_participant_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 feature 'User creates Participant', js: true do
 
   scenario 'and sees the new Participants in the list' do
+    given_i_am_viewing_the_participant_list
+    when_i_create_a_new_participant
+    then_i_should_see_the_new_participant_in_the_list
+  end
+
+  def given_i_am_viewing_the_participant_list
     protocol = create_and_assign_protocol_to_me
 
     visit protocol_path(protocol.id)
     click_link 'Participant List'
-    click_link 'Create New Participant'
-    user_fills_in_new_participant_form
-    find("input[value='Save Participant']").click
-    refresh_bootstrap_table 'table.participants'
-    expect(page).to have_css('#flashes_container', text: 'Participant Created')
-    expect(page).to have_css('table.participants tbody tr', count: 4)
   end
 
-  def user_fills_in_new_participant_form
+  def when_i_create_a_new_participant
+    click_link 'Create New Participant'
+
     participant = build(:participant_with_protocol)
 
     fill_in 'First Name', with: participant.first_name
@@ -34,5 +36,14 @@ feature 'User creates Participant', js: true do
     bootstrap_select '#participant_race', participant.race
     fill_in 'Address', with: participant.address
     fill_in 'Phone', with: participant.phone
+
+    find("input[value='Save Participant']").click
+
+    refresh_bootstrap_table 'table.participants'
+  end
+
+  def then_i_should_see_the_new_participant_in_the_list
+    expect(page).to have_css('#flashes_container', text: 'Participant Created')
+    expect(page).to have_css('table.participants tbody tr', count: 4)
   end
 end
--- spec/features/participants/participant_list/user_edits_participant_spec.rb
+++ spec/features/participants/participant_list/user_edits_participant_spec.rb
@@ -3,17 +3,31 @@ require 'rails_helper'
 feature 'User edits Participant', js: true do
 
   scenario 'and sees the updated Participant' do
+    given_i_am_viewing_the_participant_list
+    when_i_update_a_participants_details
+    then_i_should_see_the_updated_details
+  end
+
+  def given_i_am_viewing_the_participant_list
     protocol = create_and_assign_protocol_to_me
 
     visit protocol_path(protocol.id)
     click_link 'Participant List'
+  end
+
+  def when_i_update_a_participants_details
     page.find('table.participants tbody tr:first-child td.edit a').click
+
     fill_in 'First Name', with: 'Starlord'
     page.execute_script %Q{ $('#dob_time_picker').trigger("focus") }
     page.execute_script %Q{ $("td.day:contains('15')").trigger("click") }
+    
     find("input[value='Save Participant']").click
+    
     refresh_bootstrap_table 'table.participants'
+  end
 
+  def then_i_should_see_the_updated_details
     expect(page).to have_css('#flashes_container', text: 'Participant Updated')
     expect(page).to have_css('table.participants tbody tr td.first_name', text: 'Starlord')
   end
