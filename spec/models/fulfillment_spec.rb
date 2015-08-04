31f2bb3f8299761dde8ef55844fe48295791b852
Kyle Glick <kyleglick99@gmail.com>
2015-08-04 12:07:46 -0400

    added validation in spec

--- spec/models/fulfillment_spec.rb
+++ spec/models/fulfillment_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Fulfillment, type: :model do
 
     it { is_expected.to validate_presence_of(:line_item_id) }
     it { is_expected.to validate_presence_of(:fulfilled_at) }
+    it { is_expected.to validate_presence_of(:account_number) }
     it { is_expected.to validate_presence_of(:quantity) }
     it { is_expected.to validate_presence_of(:performer_id) }
     it { is_expected.to validate_numericality_of(:quantity) }
