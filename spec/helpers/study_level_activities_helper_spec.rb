cd0c16dfbda75a16d04742f73f093c0d3dc804d5
Kyle Glick <kyleglick99@gmail.com>
2015-08-05 10:25:27 -0400

    filled out study_level_activities_helper_spec

--- spec/helpers/study_level_activities_helper_spec.rb
+++ spec/helpers/study_level_activities_helper_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe StudyLevelActivitiesHelper do
+
+  describe "#components_for_select" do
+    it "should return components options" do
+      components = []
+      #No components
+      expect(helper.components_for_select(components)).to eq(components_for_select_return(components))
+
+      #Components
+      3.times do
+        components << create(:component)
+      end
+      expect(helper.components_for_select(components)).to eq(components_for_select_return(components))
+    end
+  end
+
+  def components_for_select_return components
+    if components.empty?
+      options_for_select(["This Service Has No Components"], disabled: "This Service Has No Components")
+    else
+      deleted_components = components.select{|c| c.deleted_at and c.selected } # deleted and selected
+      visible_components = deleted_components + components.select{ |c| not c.deleted_at } # (deleted and selected) or not deleted
+      options_from_collection_for_select( visible_components, 'id', 'component', selected: components.map{|c| c.id if c.selected}, disabled: deleted_components.map(&:id) )
+    end
+  end
 end
