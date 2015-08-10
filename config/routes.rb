344313dc821a9cc0d8afdc1206d40778f4974220
rmjames <robert-james-miller@uiowa.edu>
2015-08-10 10:41:28 -0500

    updated Shibboleth to be configurable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-
-  devise_for :identities, :controllers => { :omniauth_callbacks => "identities/omniauth_callbacks" }, :path_names => {:sign_in => 'auth/shibboleth' }
-    
+  
+  if ENV.fetch('USE_SHIBBOLETH_ONLY') == 'true' # Shibboleth is the only authentication option and all URLs are protected by it
+    devise_for :identities, :controllers => { :omniauth_callbacks => "identities/omniauth_callbacks" }, :path_names => {:sign_in => 'auth/shibboleth' }
+  else # add Shibboleth as an option and allow users to view 'sign in' page
+    devise_for :identities, :controllers => { :omniauth_callbacks => "identities/omniauth_callbacks" }
+  end
+  
   resources :protocols
   resources :visit_groups, only: [:new, :create, :edit, :update, :destroy]
   resources :components, only: [:update]
