8694c47af4e87b7dd45e5260a15973722bbab4e7
Charlie Thiry <thiry@musc.edu>
2015-08-13 13:43:00 -0400

    only delete reports table if table exists

--- db/migrate/20150811142301_remove_reports_table.rb
+++ db/migrate/20150811142301_remove_reports_table.rb
@@ -1,5 +1,5 @@
 class RemoveReportsTable < ActiveRecord::Migration
   def change
-    drop_table :reports
+    drop_table :reports if ActiveRecord::Base.connection.table_exists? 'reports'
   end
 end
