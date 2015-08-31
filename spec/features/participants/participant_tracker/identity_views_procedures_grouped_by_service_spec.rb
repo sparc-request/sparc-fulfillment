00c3d67bb05901346970ded8219c633ecea06ff8
Jerry Hardee <hardeeje@musc.edu>
2015-08-31 16:57:49 -0400

    Scaring up a spec.

--- spec/features/participants/participant_tracker/identity_views_procedures_grouped_by_service_spec.rb
+++ spec/features/participants/participant_tracker/identity_views_procedures_grouped_by_service_spec.rb
@@ -2,60 +2,73 @@ require 'rails_helper'
 
 feature 'Identity views Procedures grouped by Service', js: true do
 
+  before(:each) do
+    @protocol    = create_and_assign_protocol_to_me
+    @participant = Participant.first
+    @appointment = Appointment.first
+    @services    = @protocol.organization.inclusive_child_services(:per_participant)
+  end
+
   scenario 'and sees that single Services are not grouped' do
-    given_an_appointment_with_a_single_service
-    when_i_view_the_participants_calendar
+    given_an_appointment_with_unique_procedures
     then_i_should_see_a_non_grouped_services_row
   end
 
   scenario 'and sees that duplicate Services are grouped' do
-    given_an_appointment_with_duplicate_services
-    when_i_view_the_participants_calendar
+    given_an_appointment_with_duplicated_procedures
     then_i_should_see_a_grouped_services_row
   end
 
   scenario 'and sees that duplicate Services are not grouped after updating R/T' do
-    given_an_appointment_with_duplicate_services
-    when_i_view_the_participants_calendar
+    given_an_appointment_with_duplicated_procedures
     and_i_update_a_grouped_procedure_rt_out_of_the_group
     then_i_should_not_see_grouped_service_row
   end
 
   scenario 'and sees that duplicate Services are grouped after updating R/T' do
-    given_an_appointment_with_a_single_service
-    when_i_view_the_participants_calendar
+    given_an_appointment_with_unique_procedures
     and_i_update_a_grouped_procedure_rt_into_the_group
     then_i_should_see_a_grouped_services_row
   end
 
-  def given_an_appointment_with_a_single_service
-    create(:identity)
-    protocol = protocol_with_single_service
-    participant = Participant.first
-    visit participant_path(participant)
+  def given_an_appointment_with_unique_procedures
+    visit participant_path(@participant)
+    bootstrap_select '#appointment_select', @appointment.name
+    wait_for_ajax
+
+    @services.each do |service|
+      bootstrap_select '#service_list', service.name
+      find('button.add_service').click
+      wait_for_ajax
+    end
   end
 
-  def given_an_appointment_with_duplicate_services
-    create(:identity)
-    create_and_assign_protocol_with_duplicate_services
-  end
+  def given_an_appointment_with_duplicated_procedures
+    visit participant_path(@participant)
+    bootstrap_select '#appointment_select', @appointment.name
+    wait_for_ajax
 
-  def when_i_view_the_participants_calendar
-    participant = Participant.first
-    appointment = Appointment.first
-
-    visit participant_path(participant)
-    bootstrap_select '#appointment_select', appointment.name
+    3.times do
+      find('button.add_service').click
+      wait_for_ajax
+    end
   end
 
   def when_i_select_an_appointment_with_single_services
-    appointment = Appointment.first
-
-    bootstrap_select '#appointment_select', appointment.name
+    bootstrap_select '#appointment_select', @appointment.name
   end
 
   def when_i_update_a_grouped_procedures_rt
 
+  end
+
+  def and_i_update_a_grouped_procedure_rt_out_of_the_group
+    procedure = Procedure.first
+    bootstrap_select "#quantity_type_#{procedure.id}", 'T'
+    wait_for_ajax
+  end
+
+  def and_i_update_a_grouped_procedure_rt_into_the_group
   end
 
   def then_i_should_see_a_grouped_services_row
@@ -64,9 +77,5 @@ feature 'Identity views Procedures grouped by Service', js: true do
 
   def then_i_should_see_a_non_grouped_services_row
     expect(page).to_not have_css('table.procedures tbody > tr.procedure')
-  end
-
-  def then_i_should_not_see_grouped_service_row
-
   end
 end
