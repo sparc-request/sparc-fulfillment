4c9c72bab2c4229edc8a1bf4c056fe36d7b7bd08
Charlie Thiry <thiry@musc.edu>
2015-09-04 15:05:28 -0400

    add type to remove_column for rollback capability *whistle-whistle*

--- db/migrate/20150824143847_account_number_contact_to_line_item.rb
+++ db/migrate/20150824143847_account_number_contact_to_line_item.rb
@@ -1,6 +1,6 @@
 class AccountNumberContactToLineItem < ActiveRecord::Migration
   def change
-    remove_column :fulfillments, :account_number
+    remove_column :fulfillments, :account_number, :string
     add_column :line_items, :account_number, :string
     add_column :line_items, :contact_name, :string
   end
