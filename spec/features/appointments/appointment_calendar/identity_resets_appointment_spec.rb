45b7f5f0227ff17ceea13960e3b81b4fe085e5cd
Stuart Johnson <work.stuart.johnson@gmail.com>
2015-09-08 12:09:08 -0400

    adding specs for appointment reset

--- spec/features/appointments/appointment_calendar/identity_resets_appointment_spec.rb
+++ spec/features/appointments/appointment_calendar/identity_resets_appointment_spec.rb
@@ -2,29 +2,85 @@ require 'rails_helper'
 
 feature 'Identity resets appointment', js: true do
   scenario 'the appointment is completed, but is then reset' do
-    given_i_have_resolved_all_procedures_on_an_appointment_and_completed_the_appointment
+    given_i_am_viewing_a_participants_calendar_with_procedures
+    and_i_start_the_appointment
+    and_i_add_an_unscheduled_procedure
+    and_i_resolve_all_procedures
+    and_i_complete_the_visit
     and_i_click_the_reset_button
     i_should_see_a_reset_appointment
   end
 
-  def given_i_have_resolved_all_procedures_on_an_appointment_and_completed_the_appointment
-    protocol = create_and_assign_protocol_to_me
-    participant = protocol.participants.first
-    visit_group = participant.appointments.first.visit_group\
+  def given_i_am_viewing_a_participants_calendar_with_procedures
+    @protocol     = create_and_assign_blank_protocol_to_me
+    project_role  = create(:project_role_pi, protocol: @protocol)
+    arm           = create(:arm_imported_from_sparc, protocol: @protocol, visit_count: 10)
+    @participant  = Participant.first
+    @visit_group  = arm.visit_groups.first
+    @appointment  = @visit_group.appointments.where(participant_id: @participant).first
+    line_item_1   = arm.line_items[0]
+    line_item_2   = arm.line_items[1]
 
-    appointment = participant.appointments.first
-    appointment.update_attributes(start_date: Time.now, completed_date: Time.now.tomorrow)
+    visit protocol_path(@protocol.id)
+    find("#line_item_#{line_item_1.id} .check_row").click()
+    wait_for_ajax
+    find("#line_item_#{line_item_2.id} .check_row").click()
+    wait_for_ajax
 
-    service = protocol.organization.inclusive_child_services(:per_participant).first
-    procedure = create(:procedure_complete, appointment_id: appointment.id, service_name: service.name, service_id: service.id, visit_id: appointment.visit_group.visits.first.id, sparc_core_name: service.organization.name)
-    visit participant_path participant
-    bootstrap_select '#appointment_select', visit_group.name
+    visit participant_path(@participant)
+    bootstrap_select('#appointment_select', @visit_group.name)
+    wait_for_ajax
+  end
+
+  def and_i_start_the_appointment
+    find('button.start_visit').click
+    wait_for_ajax
+  end
+
+  def and_i_add_an_unscheduled_procedure
+    service     = @protocol.organization.inclusive_child_services(:per_participant).first
+
+    bootstrap_select('#service_list', service.name)
+    fill_in 'service_quantity', with: '1'
+    page.find('button.add_service').click
+    wait_for_ajax
+  end
+
+  def and_i_resolve_all_procedures
+    procedures = @appointment.procedures
+
+    find("tr[data-id='#{procedures[0].id}'] label.status.complete").click
+    wait_for_ajax
+    find("tr[data-id='#{procedures[1].id}'] label.status.incomplete").click
+    wait_for_ajax
+    bootstrap_select '.reason-select', "Assessment missed"
+    click_button 'Save'
+    wait_for_ajax
+
+    find("tr[data-id='#{procedures[2].id}'] button.followup.new").click
+    bootstrap_select '#task_assignee_id', Identity.first.full_name
+    page.execute_script %Q{ $("#follow_up_procedure_datepicker").children(".input-group-addon").trigger("click")}
+    page.execute_script %Q{ $("td.day:contains('10')").trigger("click") }
+    fill_in 'Comment', with: 'Test comment'
+    click_button 'Save'
+    wait_for_ajax
+  end
+
+  def and_i_complete_the_visit
+    find("button.complete_visit").click
     wait_for_ajax
   end
 
   def and_i_click_the_reset_button
+    accept_confirm do
+      find("button.reset_visit").click
+    end
+    wait_for_ajax
   end
 
   def i_should_see_a_reset_appointment
+    expect(page).to have_css('button.start_visit', visible: true)
+    expect(page).to have_css('button.complete_visit.disabled', visible: true)
+    expect(page). to have_css('td.status .pre_start_disabled', visible: true, count: 2)
   end
 end
