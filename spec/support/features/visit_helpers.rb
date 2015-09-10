0d6c5aa573acd6d98dbae39e1ce91b10a41b2900
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-09-10 11:20:55 -0400

    feature 'Identity adds multiple Procedures'

--- spec/support/features/visit_helpers.rb
+++ spec/support/features/visit_helpers.rb
@@ -2,10 +2,16 @@ module Features
 
   module VisitHelpers
 
+    def and_the_visit_has_one_grouped_procedure
+      2.times { add_a_procedure services.first }
+    end
+
     def add_a_procedure(service, count = 1)
-      bootstrap_select '#service_list', service.name
-      find('button.add_service').click
-      wait_for_ajax
+      count.times do
+        bootstrap_select '#service_list', service.name
+        find('button.add_service').click
+        wait_for_ajax
+      end
     end
 
     def given_i_am_viewing_a_visit
