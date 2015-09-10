16aa6fbd481f9966fb35dcc54c591462b0415f8c
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-09-10 10:35:40 -0400

    Add add_a_service helper

--- spec/support/features/visit_helpers.rb
+++ spec/support/features/visit_helpers.rb
@@ -2,6 +2,12 @@ module Features
 
   module VisitHelpers
 
+    def add_a_service(service, count = 1)
+      bootstrap_select '#service_list', service.name
+      find('button.add_service').click
+      wait_for_ajax
+    end
+
     def given_i_am_viewing_a_visit
       visit participant_path participant
       bootstrap_select '#appointment_select', appointment.name
