e3e00e13006163082146109f0fc5e0ea91b834e5
kyle-glick <kyleglick99@gmail.com>
2015-09-25 09:37:23 -0400

    Update identity_adds_multiple_procedures_spec.rb

--- spec/features/appointments/appointment_calendar/grouped_services/identity_adds_multiple_procedures_spec.rb
+++ spec/features/appointments/appointment_calendar/grouped_services/identity_adds_multiple_procedures_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 feature 'Identity adds multiple Procedures', js: true do
 
   let!(:protocol)    { create_and_assign_protocol_to_me }
-  let!(:participant) { Participant.first }
-  let!(:appointment) { Appointment.first }
+  let!(:participant) { protocol.participants.first }
+  let!(:appointment) { participant.appointments.first }
   let!(:services)    { protocol.organization.inclusive_child_services(:per_participant) }
 
   scenario 'which are not part of an existing group' do
