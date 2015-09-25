7bfb48da60b65f168b556f3a41b6c4353b8f02c9
Kyle Glick <kyleglick99@gmail.com>
2015-09-25 16:32:53 -0400

    parentheses, indention

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,6 +1,6 @@
 module DocumentsHelper
 
-  def format_document_date date
+  def format_document_date(date)
     if date.present?
       date.strftime(t(:documents)[:date_time_formatter_ruby])
     else
@@ -8,7 +8,7 @@ module DocumentsHelper
     end
   end
 
-  def attached_file_formatter document
+  def attached_file_formatter(document)
     if document.completed?
       content_tag(:a, class: 'attached_file', id: "file_#{document.id}", href: document_path(document), target: :blank, title: 'Download File', 'data-id' => document.id) do
         content_tag(:span, '', class: 'glyphicon glyphicon-file')
@@ -18,7 +18,7 @@ module DocumentsHelper
     end
   end
 
-  def edit_formatter document
+  def edit_formatter(document)
     [
       "<a class='edit edit-document ml10' href='javascript:void(0)' title='Edit' data-document_id='#{document.id}'>",
       "<i class='glyphicon glyphicon-edit'></i>",
@@ -26,15 +26,15 @@ module DocumentsHelper
     ].join ""
   end
 
-  def delete_formatter document
+  def delete_formatter(document)
     if document.completed?
     [
       "<a class='remove remove-document' href='javascript:void(0)' title='Remove' data-document_id='#{document.id}'>",
       "<i class='glyphicon glyphicon-remove'></i>",
       "</a>"
     ].join ""
-  else
-    "<i class='glyphicon glyphicon-remove' style='cursor:default'></i>"
-  end
+    else
+      "<i class='glyphicon glyphicon-remove' style='cursor:default'></i>"
+    end
   end
 end
