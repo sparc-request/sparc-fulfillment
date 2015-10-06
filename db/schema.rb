df105940211d32c9c52b5211abed693c6a637549
Andrew Cates <amcates@gmail.com>
2015-10-06 12:28:10 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -275,42 +275,6 @@ ActiveRecord::Schema.define(version: 20150824143847) do
   add_index "tasks", ["assignee_id"], name: "index_tasks_on_assignee_id", using: :btree
   add_index "tasks", ["identity_id"], name: "index_tasks_on_identity_id", using: :btree
 
-  create_table "user_roles", force: :cascade do |t|
-    t.integer  "user_id",     limit: 4
-    t.integer  "protocol_id", limit: 4
-    t.string   "rights",      limit: 255
-    t.string   "role",        limit: 255
-    t.string   "role_other",  limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "deleted_at"
-  end
-
-  add_index "user_roles", ["protocol_id"], name: "index_user_roles_on_protocol_id", using: :btree
-  add_index "user_roles", ["user_id"], name: "index_user_roles_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",                           null: false
-    t.string   "encrypted_password",     limit: 255, default: "",                           null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,                            null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "time_zone",              limit: 255, default: "Eastern Time (US & Canada)"
-    t.integer  "tasks_count",            limit: 4,   default: 0
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   create_table "versions", force: :cascade do |t|
     t.string   "item_type",  limit: 255,   null: false
     t.integer  "item_id",    limit: 4,     null: false
