dda96940514e38a1f6f24e88b634e935c5825e46
Stuart Johnson <work.stuart.johnson@gmail.com>
2015-11-11 14:49:33 -0500

    schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824143847) do
+ActiveRecord::Schema.define(version: 20151015141652) do
 
   create_table "appointment_statuses", force: :cascade do |t|
     t.string   "status",         limit: 255
