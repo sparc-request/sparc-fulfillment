93ba80f03224d7cc56350429c04df800c1474f48
Johanna Wiel <jwiel86@gmail.com>
2015-11-19 16:35:07 -0500

    added helper spec

--- spec/helpers/study_level_activities_helper_spec.rb
+++ spec/helpers/study_level_activities_helper_spec.rb
@@ -19,4 +19,12 @@ RSpec.describe StudyLevelActivitiesHelper do
       expect(val.include? 'value="3"')
     end
   end
+
+  describe "is protocol of type study?" do
+    it "should return the protocol type" do
+      protocol = create_and_assign_protocol_to_me
+      is_study = helper.is_protocol_type_study? (protocol)
+      expect(is_study).to eq Sparc::Protocol.where(id: protocol.sparc_id).first.type == 'Study'
+    end
+  end
 end
