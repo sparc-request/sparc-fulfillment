b457ae89ec92a42440ec5a829dfef23b02d9ef32
kyle-glick <kyleglick99@gmail.com>
2016-02-18 16:06:27 -0500

    Update visit_group_helper.rb

--- app/helpers/visit_group_helper.rb
+++ app/helpers/visit_group_helper.rb
@@ -1,6 +1,6 @@
 module VisitGroupHelper
 
   def vg_add_as_last_option arm
-    content_tag(:option, "Add as last", value: arm.visit_groups.last.position + 1)
+    content_tag(:option, "Add as last", value: arm.visit_groups.size + 1)
   end
 end
