9354b91e575539cb81e91a5a4c9fe9b64680d781
Kyle Glick <kyleglick99@gmail.com>
2016-02-25 12:09:40 -0500

    identity_views_reports_tab_spec

--- spec/features/protocols/identity_views_reports_tab_spec.rb
+++ spec/features/protocols/identity_views_reports_tab_spec.rb
@@ -14,19 +14,12 @@ feature 'Identity views Report tab', js: true, enqueue: false do
     then_i_should_see_a_report_in_the_participant_reports_table
   end
 
- #  scenario 'then changes the name of a report' do
- #    given_i_have_created_a_study_schedule_report
- #    when_i_view_the_reports_tab
- #    then_i_change_the_title_of_the_report
- # end
-
   def given_i_have_created_a_study_schedule_report
     protocol = create_and_assign_protocol_to_me
 
     visit protocol_path protocol
     
-    find("a#study_schedule_report_#{protocol.id.to_s}").click
-    wait_for_ajax
+    create(:document_of_protocol_report, documentable_id: protocol.id)
   end
 
   def given_i_have_created_a_participant_report
@@ -35,23 +28,12 @@ feature 'Identity views Report tab', js: true, enqueue: false do
 
     visit protocol_path protocol
 
-    click_link 'Participant Tracker'
-    
-    find("a#participant_report_#{participant.id.to_s}").click
-    wait_for_ajax    
+    create(:document_of_participant_report, documentable_id: protocol.id)
   end
 
   def when_i_view_the_reports_tab
     click_link 'Reports'
   end
-
-  # def then_i_change_the_title_of_the_report
-  #   find(".edit-document").click
-  #   fill_in "#document_title", with: "test title"
-  #   click "Save"
-  #   wait_for_ajax
-  #   expect(page).to have_content "test title"
-  # end
 
   def then_i_should_see_a_report_in_the_protocol_reports_table
     expect(page).to have_css('table.protocol_reports tbody td.title', count: 1)
