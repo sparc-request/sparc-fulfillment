0d00430c85f8e3833d77e749ba83a7ba326828a4
William Holt <holtw@musc.edu>
2016-04-01 15:22:58 -0400

    removed commented out code

--- spec/controllers/multiple_line_items_controller_spec.rb
+++ spec/controllers/multiple_line_items_controller_spec.rb
@@ -2,20 +2,6 @@ require 'rails_helper'
 
 RSpec.describe MultipleLineItemsController, type: :controller do
 
-  # def create_and_assign_protocol_to_me
-  #   identity              = Identity.first
-  #   sub_service_request   = create(:sub_service_request_with_organization)
-  #   protocol              = create(:protocol_imported_from_sparc, sub_service_request: sub_service_request)
-  #   organization_provider = create(:organization_provider, name: "Provider")
-  #   organization_program  = create(:organization_program, name: "Program", parent: organization_provider)
-  #   organization          = sub_service_request.organization
-  #   organization.update_attributes(parent: organization_program, name: "Core")
-  #   FactoryGirl.create(:clinical_provider, identity: identity, organization: organization)
-  #   FactoryGirl.create(:project_role_pi, identity: identity, protocol: protocol)
-
-  #   protocol
-  # end
-
   login_user
 
   describe "GET #new_line_items" do
