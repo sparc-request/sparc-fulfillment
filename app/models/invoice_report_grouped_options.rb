27e6ccf2e9e1b3b8047e2a99654c7ad3a1ca9e1a
Johanna Wiel <jwiel86@gmail.com>
2016-04-14 17:01:04 -0400

    refactored invoice_report_grouped_options

--- app/models/invoice_report_grouped_options.rb
+++ app/models/invoice_report_grouped_options.rb
@@ -5,7 +5,10 @@ class InvoiceReportGroupedOptions
   end
 
   def collect_grouped_options
-    @grouped_options = group_organizations(@organizations) || []
+    group_organizations(@organizations, "Institutions")
+    group_organizations(@organizations, "Providers")
+    group_organizations(@organizations, "Programs")
+    group_organizations(@organizations, "Cores")
   end
 
   private
@@ -15,34 +18,12 @@ class InvoiceReportGroupedOptions
   end
 
   def organization_to_array(organizations, type)
-    case type
-      when "Institution"
-        institution_array = ['Institutions', organizations.flatten.map { |institution| [institution.name, institution.id] }] unless organizations.flatten.empty?
-        add_array_to_grouped_options(institution_array)
-      when "Provider"
-        provider_array = ['Providers', organizations.flatten.map { |provider| [provider.name, provider.id] }] unless organizations.flatten.empty?
-        add_array_to_grouped_options(provider_array)
-      when "Program"
-        program_array = ['Programs', organizations.flatten.map { |program| [program.name, program.id] }] unless organizations.flatten.empty?
-        add_array_to_grouped_options(program_array)
-      when "Core"
-        core_array = ['Cores', organizations.map { |core| [core.name, core.id] }] unless organizations.flatten.empty?
-        add_array_to_grouped_options(core_array) 
-    end
+    org_array = [type, organizations.flatten.map { |org| [org.name, org.id] }] unless organizations.flatten.empty?
+    add_array_to_grouped_options(org_array)
   end
 
-  def group_organizations(organizations)
-
-    institutions = organizations.select{|org| org.type == "Institution"}
-    organization_to_array(institutions, "Institution")
-
-    providers = organizations.select{|org| org.type == "Provider"}
-    organization_to_array(providers, "Provider")
-
-    programs = organizations.select{|org| org.type == "Program"}
-    organization_to_array(programs, "Program")
-
-    cores = organizations.select{|org| org.type == "Core"}
-    organization_to_array(cores, "Core")
+  def group_organizations(organizations, type)
+    organizations = organizations.select{|org| org.type == type.chomp('s')}
+    organization_to_array(organizations, type)
   end
 end
