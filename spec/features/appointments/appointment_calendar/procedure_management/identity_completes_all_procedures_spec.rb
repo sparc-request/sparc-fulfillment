23fd3e8b3c6618fba89b9ae7d128b8e09e7f4970
Johanna Wiel <jwiel86@gmail.com>
2016-04-25 15:29:42 -0400

    expanded the feature spec for identity completes all

--- spec/features/appointments/appointment_calendar/procedure_management/identity_completes_all_procedures_spec.rb
+++ spec/features/appointments/appointment_calendar/procedure_management/identity_completes_all_procedures_spec.rb
@@ -11,14 +11,6 @@ feature 'Identity completes all Procedures', js: true do
     then_all_the_procedure_complete_buttons_should_be_active
   end
 
-  scenario 'and sees the procedures are completed' do
-    given_i_have_added_n_procedures_to_an_appointment_such_that_n_is 2
-    when_i_complete_all_the_procedures
-    then_i_should_see_a_complete_all_modal
-    when_i_save_the_modal
-    then_all_procedures_should_be_completed
-  end
-
   scenario 'leaves defaults and sees the procedures are completed and updated with defaults' do
     given_i_have_added_n_procedures_to_an_appointment_such_that_n_is 2
     when_i_complete_all_the_procedures
@@ -26,7 +18,7 @@ feature 'Identity completes all Procedures', js: true do
     with_a_default_completed_date_of_current_date
     and_with_a_default_performed_by_name_of_current_user
     when_i_save_the_modal
-    then_all_procedures_should_be_completed
+    and_i_unroll_the_procedures_accordion
     and_all_procedures_should_have_completed_date_set_to_default
     and_all_procedures_should_have_performed_by_set_to_default
   end
@@ -38,24 +30,25 @@ feature 'Identity completes all Procedures', js: true do
     when_i_edit_the_default_date
     and_when_i_edit_the_default_performer
     when_i_save_the_modal
-    then_all_procedures_should_be_completed
+    and_i_unroll_the_procedures_accordion
     and_all_procedures_should_have_selected_completed_date
     and_all_procedures_should_have_selected_performer
   end
 
   def given_i_have_added_n_procedures_to_an_appointment_such_that_n_is number_of_procedures
-    @current_user         = create(:identity)
-    @clinical_provider    = create(:identity)
+    @identity1              = Identity.first
+    @identity2              = create(:identity, first_name: "Sassy")
     sub_service_request   = create(:sub_service_request_with_organization)
     protocol              = create(:protocol_imported_from_sparc, sub_service_request: sub_service_request)
     organization_provider = create(:organization_provider, name: "Provider")
     organization_program  = create(:organization_program, name: "Program", parent: organization_provider)
     organization          = sub_service_request.organization
     organization.update_attributes(parent: organization_program, name: "Core")
-    FactoryGirl.create(:clinical_provider, identity: @clinical_provider, organization: organization)
-    FactoryGirl.create(:clinical_provider, identity: @current_user, organization: organization)
-    FactoryGirl.create(:project_role_pi, identity: @current_user, protocol: protocol)
+    FactoryGirl.create(:clinical_provider, identity: @identity1, organization: organization)
+    FactoryGirl.create(:project_role_pi, identity: @identity1, protocol: protocol)
 
+    FactoryGirl.create(:clinical_provider, identity: @identity2, organization: organization)
+    FactoryGirl.create(:project_role_pi, identity: @identity2, protocol: protocol)
     
     @participant = protocol.participants.first
     visit_group  = @participant.appointments.first.visit_group
@@ -87,7 +80,7 @@ feature 'Identity completes all Procedures', js: true do
   end
 
   def and_with_a_default_performed_by_name_of_current_user
-    expect(page).to have_css('.modal-performed-by .performed-by-dropdown', text: "#{@identity.full_name}")
+    expect(page).to have_css('.modal-performed-by .performed-by-dropdown', text: "#{@identity1.full_name}")
   end
 
   def when_i_save_the_modal
@@ -102,38 +95,49 @@ feature 'Identity completes all Procedures', js: true do
     expect(page).to have_css('label.status.complete.active', count: 2)
   end
 
-  def then_all_procedures_should_be_completed
-    wait_for_ajax
-    expect(@participant.procedures.first.status).to eq("complete")
-    expect(@participant.procedures.last.status).to eq("complete")
-  end
-
   def and_all_procedures_should_have_completed_date_set_to_default
-    expect(@participant.procedures.first.completed_date).to eq(Time.current.strftime('%m-%d-%Y'))
-    expect(@participant.procedures.last.completed_date).to eq(Time.current.strftime('%m-%d-%Y'))
+ 
+    procedure1_date = page.evaluate_script %Q{ $("tr.procedure[data-id='1'] div.completed_date_field input.datetimepicker").val(); }
+    procedure2_date = page.evaluate_script %Q{ $("tr.procedure[data-id='2'] div.completed_date_field input.datetimepicker").val(); }
+  
+    expect(procedure1_date).to eq((Time.current.strftime('%m-%d-%Y')))
+    expect(procedure2_date).to eq((Time.current.strftime('%m-%d-%Y')))
   end
 
   def and_all_procedures_should_have_performed_by_set_to_default
-    expect(@participant.procedures.first.performed_by).to eq(@identity.id)
-    expect(@participant.procedures.last.performed_by).to eq(@identity.id)
+    procedure1_performed_by = page.evaluate_script %Q{ $("tr.procedure[data-id='1'] td.performed-by .selectpicker").val() }
+    procedure2_performed_by = page.evaluate_script %Q{ $("tr.procedure[data-id='2'] td.performed-by .selectpicker").val() }
+
+    expect(procedure1_performed_by.to_i).to eq(@identity1.id)
+    expect(procedure2_performed_by.to_i).to eq(@identity1.id)
+
   end
 
   def when_i_edit_the_default_date
     page.execute_script %Q{ $(".datetimepicker").siblings(".input-group-addon").trigger("click")}
-    page.execute_script %Q{ $("td.day:contains('15')").trigger("click") }
+    tomorrow_day = evaluate_script %Q{ parseInt($("td.day.today").html()) + 1 }
+    page.execute_script %Q{ $("td.day:contains(#{tomorrow_day})").trigger("click") }
     wait_for_ajax
   end
 
   def and_when_i_edit_the_default_performer
-    bootstrap_select ".performed-by-dropdown", @performer.full_name
+    bootstrap_select ".performed-by-dropdown", @identity2.full_name
   end
 
   def and_all_procedures_should_have_selected_completed_date
-    expect(@participant.procedures.first.completed_date).to eq(Time.current.strftime('%m-15-%Y'))
-    expect(@participant.procedures.last.completed_date).to eq(Time.current.strftime('%m-15-%Y'))
+    procedure1_date = page.evaluate_script %Q{ $("tr.procedure[data-id='1'] div.completed_date_field input.datetimepicker").val(); }
+    procedure2_date = page.evaluate_script %Q{ $("tr.procedure[data-id='2'] div.completed_date_field input.datetimepicker").val(); }
+  
+    expect(procedure1_date).to eq((Date.current.tomorrow.strftime('%m-%d-%Y')))
+    expect(procedure2_date).to eq((Date.current.tomorrow.strftime('%m-%d-%Y')))
   end
 
   def and_all_procedures_should_have_selected_performer
+    procedure1_performed_by = page.evaluate_script %Q{ $("tr.procedure[data-id='1'] td.performed-by .selectpicker").val() }
+    procedure2_performed_by = page.evaluate_script %Q{ $("tr.procedure[data-id='2'] td.performed-by .selectpicker").val() }
+
+    expect(procedure1_performed_by.to_i).to eq(@identity2.id)
+    expect(procedure2_performed_by.to_i).to eq(@identity2.id)
   end
 
 end
