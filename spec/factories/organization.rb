637d52a83ab2cdb4a92b2a2dc2adc8483e3a4ef2
Johanna Wiel <jwiel86@gmail.com>
2016-04-28 15:05:01 -0400

    spruced up organization spec per pull request comments

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -4,9 +4,37 @@ FactoryGirl.define do
     sequence(:name) { |n| "Fake Organization #{n}" }  # need this for fake data
     process_ssrs false
 
+    transient do
+      children_count 0
+      has_protocols false
+    end
+
     after :create do |organization, evaluator|
       create_list(:pricing_setup, 3, organization: organization).each do |pricing_setup|
         pricing_setup.update_attributes(organization_id: organization.id)
+      end
+
+      evaluator.children_count.times do
+        case(organization.type)
+          when "Institution"
+            if evaluator.has_protocols
+              create(:organization_provider_with_protocols, parent_id: organization.id)
+            else
+              create(:organization_provider, parent_id: organization.id)
+            end
+          when "Provider"
+            if evaluator.has_protocols
+              create(:organization_program_with_protocols, parent_id: organization.id)
+            else
+              create(:organization_program, parent_id: organization.id)
+            end
+          when "Program"
+            if evaluator.has_protocols
+              create(:organization_core_with_protocols, parent_id: organization.id)
+            else
+              create(:organization_core, parent_id: organization.id)
+            end
+        end
       end
     end
 
@@ -29,7 +57,8 @@ FactoryGirl.define do
 
     trait :with_protocols do
       after(:create) do |organization, evaluator|
-        create(:sub_service_request, organization: organization)
+        sub_service_request = create(:sub_service_request, organization: organization)
+        create(:protocol, sub_service_request: sub_service_request)
       end
     end
 
@@ -50,18 +79,26 @@ FactoryGirl.define do
       end
     end
 
-    trait :with_3_child_orgs do
-      after(:create) do |organization, evaluator|
-        3.times do
-          create(:organization_with_services, parent: organization)
-        end
-      end
-    end
+    # trait :with_3_child_orgs do
+    #   after(:create) do |organization, evaluator|
+    #     3.times do
+    #       create(:organization_with_services, parent: organization)
+    #     end
+    #   end
+    # end
 
     factory :organization_institution, traits: [:institution]
-    factory :organization_core, traits: [:core]
-    factory :organization_program, traits: [:program]
     factory :organization_provider, traits: [:provider]
+    factory :organization_program, traits: [:program]
+    factory :organization_core, traits: [:core]
+
+
+    factory :organization_institution_with_protocols, traits: [:institution, :with_protocols]
+    factory :organization_provider_with_protocols, traits: [:provider, :with_protocols]
+    factory :organization_program_with_protocols, traits: [:program, :with_protocols]
+    factory :organization_core_with_protocols, traits: [:core, :with_protocols]
+    
+    
     factory :organization_with_protocols, traits: [:core, :with_protocols]
 
     factory :provider_with_protocols, traits: [:provider, :with_protocols]
@@ -74,9 +111,9 @@ FactoryGirl.define do
     factory :program_with_child_organizations, traits: [:with_services, :program, :with_child_organizations]
     factory :organization_with_child_organizations, traits: [:with_services, :core, :with_child_organizations]
 
-    factory :provider_with_3_child_orgs, traits: [:with_services, :provider, :with_3_child_orgs]
-    factory :program_with_3_child_orgs, traits: [:with_services, :program, :with_3_child_orgs]
-    factory :core_with_3_child_orgs, traits: [:with_services, :core, :with_3_child_orgs]
+    # factory :provider_with_3_child_orgs, traits: [:with_services, :provider, :with_3_child_orgs]
+    # factory :program_with_3_child_orgs, traits: [:with_services, :program, :with_3_child_orgs]
+    # factory :core_with_3_child_orgs, traits: [:with_services, :core, :with_3_child_orgs]
 
   end
 end
--- spec/models/organization/organization_spec.rb
+++ spec/models/organization/organization_spec.rb
@@ -8,43 +8,95 @@ RSpec.describe Organization, type: :model do
 
 
   describe '#all_child_organizations' do
+    context "organizations with children" do
+      before :each do
+        # Creates a parent organization at the institution level with n = 2 provider children organizations
+        @institution_organization = create(:organization_institution, children_count: 2)
+        # Creates a provider organization whose parent is institution_organization who has n = 2 program children
+        @provider_organization = create(:organization_provider, children_count: 2, parent_id: @institution_organization.id )
+        # Creates a program organization whose parent is provider_organization who has n = 2 core children
+        @program_organization = create(:organization_program, children_count: 2, parent_id: @provider_organization.id )
+      end 
 
-    it "should return all child organizations" do
+      it "should return all child organizations for institution" do
+        institution_organization_children_ids = [
+                                                  @institution_organization.children.map(&:id),
+                                                  @provider_organization.children.map(&:id),
+                                                  @program_organization.children.map(&:id)
+                                                ].flatten
 
-      institution_organization = create(:organization_institution)
+        expect(@institution_organization.all_child_organizations.map(&:id).sort).to eq(institution_organization_children_ids)
+      end
 
-      provider_organization = create(:provider_with_3_child_orgs)
-      provider_organization.update_attribute(:parent_id, institution_organization.id)
+      it "should return all child organizations for provider" do
+        provider_organization_children_ids = [
+                                              @provider_organization.children.map(&:id),
+                                              @program_organization.children.map(&:id)
+                                            ].flatten
 
-      program_organization = create(:program_with_3_child_orgs)
-      program_organization.update_attribute(:parent_id, provider_organization.id)
+        expect(@provider_organization.all_child_organizations.map(&:id).sort).to eq(provider_organization_children_ids)
+      end
 
-      core_organization = create(:core_with_3_child_orgs)
-      core_organization.update_attribute(:parent_id, program_organization.id)
+      it "should return all child organizations for program" do
+        program_organization_children_ids = [ @program_organization.children.map(&:id) ].flatten
 
-      expect(program_organization.all_child_organizations.map(&:id).length).to eq(7)
+        expect(@program_organization.all_child_organizations.map(&:id).sort).to eq(program_organization_children_ids)
+      end
+    end
+    context "organizations with no children" do
+      it "should return an empty array for program with no children" do
+        program_organization = create(:organization_program)
+        expect(program_organization.all_child_organizations.map(&:id).sort).to eq([])
+      end
     end
   end
 
   describe '#child_orgs_with_protocols' do
 
-    it "should return all child organizations WITH protocols" do
+    context "children organizations with protocols" do
+      before :each do
+        # Creates a parent organization at the institution level with n = 2 provider children organizations with protocols
+        @institution_organization = create(:organization_institution, children_count: 2, has_protocols: true)
+        # Creates a provider organization whose parent is institution_organization who has n = 2 program children with protocols
+        @provider_organization = create(:organization_provider, children_count: 2, parent_id: @institution_organization.id, has_protocols: true)
+        # Creates a program organization whose parent is provider_organization who has n = 2 core children with protocols
+        @program_organization = create(:organization_program, children_count: 2, parent_id: @provider_organization.id, has_protocols: true )
+      end 
 
-      institution_organization = create(:organization_institution)
+      it "should return all child organizations for institution with protocols" do
+        
+        institution_organization_children_ids = []
+        @institution_organization.children.map { |child| institution_organization_children_ids << child.id if child.protocols.any? }
+        @provider_organization.children.map { |child| institution_organization_children_ids << child.id if child.protocols.any? }
+        @program_organization.children.map { |child| institution_organization_children_ids << child.id if child.protocols.any? }
 
-      provider_organization = create(:provider_with_3_child_orgs)
-      provider_organization.update_attribute(:parent_id, institution_organization.id)
+        institution_organization_children_ids.compact.flatten                               
 
-      program_organization = create(:program_with_3_child_orgs)
-      program_organization.update_attribute(:parent_id, provider_organization.id)
+        expect(@institution_organization.child_orgs_with_protocols.map(&:id).sort).to eq(institution_organization_children_ids)
+      end
 
-      core_organization = create(:core_with_3_child_orgs)
-      core_organization.update_attribute(:parent_id, program_organization.id)
+      it "should return all child organizations for provider" do
+        provider_organization_children_ids = []
+        @provider_organization.children.map { |child| provider_organization_children_ids << child.id if child.protocols.any? }
+        @program_organization.children.map { |child| provider_organization_children_ids << child.id if child.protocols.any? }
+        
 
-      program_sub_service_request = create(:sub_service_request, organization: program_organization)
-      program_protocol            = create(:protocol, sub_service_request: program_sub_service_request)
+        expect(@provider_organization.child_orgs_with_protocols.map(&:id).sort).to eq(provider_organization_children_ids)
+      end
 
-      expect(provider_organization.child_orgs_with_protocols.map(&:id)).to eq([program_organization.id])
+      it "should return all child organizations for program" do
+        program_organization_children_ids = []
+        @program_organization.children.map { |child| program_organization_children_ids << child.id if child.protocols.any? }
+
+        expect(@program_organization.child_orgs_with_protocols.map(&:id).sort).to eq(program_organization_children_ids)
+      end
+    end
+
+    context "children organizations with no protocols" do
+      it "should return an empty array for organization with two children that have no protocols " do
+        program_organization_with_children_with_no_protocols = create(:organization_program, children_count: 2)
+        expect(program_organization_with_children_with_no_protocols.child_orgs_with_protocols.map(&:id).sort).to eq([])
+      end
     end
   end
 end
