d3da753c746bac9a15c7dc19aa2e526f4ad679f8
Stuart Johnson <work.stuart.johnson@gmail.com>
2016-04-28 11:04:41 -0400

    syntax change

--- db/migrate/20160420131934_remove_service_cost_from_incomplete_procedures.rb
+++ db/migrate/20160420131934_remove_service_cost_from_incomplete_procedures.rb
@@ -1,6 +1,6 @@
 class RemoveServiceCostFromIncompleteProcedures < ActiveRecord::Migration
   def change
-    Procedure.where("service_cost != ? AND status != ?", false, 'complete').find_each do |procedure|
+    Procedure.where.not(service_cost: false, status: "complete").find_each do |procedure|
       procedure.update_attribute(:service_cost, nil)
     end
   end
