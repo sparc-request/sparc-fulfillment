a425129bb86182e8c54681808262912c578e3481
Johanna Wiel <jwiel86@gmail.com>
2016-04-29 10:29:21 -0400

    removed unnecessary spec and refined the identity_spec

--- spec/models/invoice_report_grouped_options_spec.rb
+++ spec/models/invoice_report_grouped_options_spec.rb
@@ -8,17 +8,12 @@ RSpec.describe InvoiceReportGroupedOptions do
 
 
       it 'should return an array with providers, programs, and cores' do
-        provider_with_protocols = create(:provider_with_protocols)
-        program_with_protocols = create(:program_with_protocols)
-        core_with_protocols = create(:core_with_protocols)
+        provider_with_protocols = create(:provider_with_protocols, name: "Gryffindor")
+        program_with_protocols = create(:program_with_protocols, name: "Slytherin")
+        core_with_protocols = create(:core_with_protocols, name: "Hufflepuff")
 
-        provider_name = provider_with_protocols.name
         provider_id = ("#{provider_with_protocols.id}").to_i
-
-        program_name = program_with_protocols.name
         program_id = ("#{program_with_protocols.id}").to_i
-
-        core_name = core_with_protocols.name
         core_id = ("#{core_with_protocols.id}").to_i
 
         organizations = [
@@ -27,7 +22,7 @@ RSpec.describe InvoiceReportGroupedOptions do
           core_with_protocols,
         ]
     
-        expect(InvoiceReportGroupedOptions.new(organizations).collect_grouped_options).to eq([["Providers", [["#{provider_name}", provider_id]]], ["Programs", [["#{program_name}", program_id]]], ["Cores", [["#{core_name}", core_id]]]])
+        expect(InvoiceReportGroupedOptions.new(organizations).collect_grouped_options).to eq([["Providers", [["Gryffindor", provider_id]]], ["Programs", [["Slytherin", program_id]]], ["Cores", [["Hufflepuff", core_id]]]])
      
       end 
     end
@@ -36,22 +31,18 @@ RSpec.describe InvoiceReportGroupedOptions do
 
 
       it 'should return an array with only programs and cores' do
-        
-        program_with_protocols = create(:program_with_protocols)
-        core_with_protocols = create(:core_with_protocols)
+        program_with_protocols = create(:organization_program, has_protocols: true, name: "Slytherin")
+        core_with_protocols = create(:organization_core, has_protocols: true, name: "Hufflepuff")
 
-        program_name = program_with_protocols.name
         program_id = ("#{program_with_protocols.id}").to_i
-
-        core_name = core_with_protocols.name
         core_id = ("#{core_with_protocols.id}").to_i
 
         organizations = [
           program_with_protocols,
-          core_with_protocols,
+          core_with_protocols
         ]
     
-        expect(InvoiceReportGroupedOptions.new(organizations).collect_grouped_options).to eq([["Programs", [["#{program_name}", program_id]]], ["Cores", [["#{core_name}", core_id]]]])
+        expect(InvoiceReportGroupedOptions.new(organizations).collect_grouped_options).to eq([["Programs", [["Slytherin", program_id]]], ["Cores", [["Hufflepuff", core_id]]]])
      
       end 
     end
