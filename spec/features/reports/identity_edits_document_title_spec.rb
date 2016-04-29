264198ac512d24f284beba9b5cc0b5778509e7b5
Johanna Wiel <jwiel86@gmail.com>
2016-04-29 14:30:37 -0400

    fixed identity_edits_spec per PR comments

--- spec/features/reports/identity_edits_document_title_spec.rb
+++ spec/features/reports/identity_edits_document_title_spec.rb
@@ -57,14 +57,14 @@ feature 'Identity edits document title', js: true, enqueue: false do
     fill_in 'End Date', with: Date.tomorrow.strftime("%m-%d-%Y")
 
     # close calendar thing, so it's not covering protocol dropdown
-    first('.modal-header').click
+    find(".modal-header", match: :first).click
     wait_for_ajax
 
     find('button.multiselect').click
     check(@protocol.organization.name)
 
     # close organization dropdown, so it's not covering protocol dropdown
-    first('.modal-header').click
+    find(".modal-header", match: :first).click
     wait_for_ajax
 
     bootstrap_select '#protocol_ids', @protocol.short_title_with_sparc_id
