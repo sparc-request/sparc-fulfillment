14cdf2b71401303d2c13da8cb94055084f3f5765
Johanna Wiel <jwiel86@gmail.com>
2016-06-17 11:00:39 -0400

    .

--- spec/features/study_schedule/management/identity_edits_visit_groups_spec.rb
+++ spec/features/study_schedule/management/identity_edits_visit_groups_spec.rb
@@ -5,12 +5,6 @@ feature 'Identity edits visit groups for a particular protocol', js: true do
   context "User adds a visit group to an arm" do
     scenario "and sees the visit group on the arm" do
       given_i_am_viewing_an_arm_with_multiple_visit_groups
-
-      @original_visit_group_1 = @arm.visit_groups.first
-      @original_visit_group_2 = @arm.visit_groups.second
-      @original_visit_group_1.day = 1
-      @original_visit_group_2.day = 3
-
       when_i_click_the_add_visit_group_button
       when_i_fill_in_the_form(day: @arm.visit_groups.last.day + 100)
       when_i_click_the_add_submit_button
@@ -19,12 +13,6 @@ feature 'Identity edits visit groups for a particular protocol', js: true do
 
     scenario "and sees a flash notification" do
       given_i_am_viewing_an_arm_with_multiple_visit_groups
-      
-      @original_visit_group_1 = @arm.visit_groups.first
-      @original_visit_group_2 = @arm.visit_groups.second
-      @original_visit_group_1.day = 1
-      @original_visit_group_2.day = 3
-
       when_i_click_the_add_visit_group_button
       when_i_fill_in_the_form(day: @arm.visit_groups.last.day + 100)
       when_i_click_the_add_submit_button
@@ -36,12 +24,11 @@ feature 'Identity edits visit groups for a particular protocol', js: true do
 
       @original_visit_group_1 = @arm.visit_groups.first
       @original_visit_group_2 = @arm.visit_groups.second
-      @original_visit_group_1.day = 1
-      @original_visit_group_2.day = 3
+
       when_i_click_the_add_visit_group_button
       when_i_fill_in_the_form(position: "Before #{@arm.visit_groups.second.name} (Day #{@arm.visit_groups.second.day})", day: @arm.visit_groups.second.day-1)
       when_i_click_the_add_submit_button
-      then_i_should_see_the_position_is 2
+      then_i_should_see_the_position_is 1
     end
   end
 
@@ -100,11 +87,10 @@ feature 'Identity edits visit groups for a particular protocol', js: true do
   end
 
   def given_i_am_viewing_an_arm_with_multiple_visit_groups
-    Protocol.destroy_all
-    Arm.destroy_all
-    VisitGroup.destroy_all
-
     @protocol = create_and_assign_protocol_to_me
+    @protocol.arms.each do |arm|
+      arm.destroy
+    end
 
     @arm      = create(:arm_with_visit_groups, visit_count: 2, protocol: @protocol, subject_count: 3)
     @visit_groups = @arm.visit_groups
@@ -156,9 +142,6 @@ feature 'Identity edits visit groups for a particular protocol', js: true do
   end
 
   def when_i_click_the_add_submit_button
-    puts "*"*50
-    puts @arm.visit_groups.inspect
-    puts @arm.visit_groups.count
     click_button 'Add'
     wait_for_ajax
   end
@@ -186,16 +169,13 @@ feature 'Identity edits visit groups for a particular protocol', js: true do
   end
 
   def then_i_should_see_the_visit_group
-    puts "^"*50
-    puts @arm.visit_groups.inspect
-    puts @arm.visit_groups.count
     find("input[value='VG']")
     expect(page).to have_css("input[value='VG']")
   end
 
   def then_i_should_see_the_updated_visit_group
     vg_id = @arm.visit_groups.first.id
-    expect(find("#visit_group_#{vg_id}").value).to eq(@arm.visit_groups.first.name)
+    expect(find("#visit_group_#{vg_id}").value).to eq("VG 2")
   end
 
   def then_i_should_not_see_the_visit_group
@@ -205,6 +185,7 @@ feature 'Identity edits visit groups for a particular protocol', js: true do
   def then_i_should_see_the_position_is position
     wait_for_ajax
     @new_visit_group = @arm.visit_groups.find_by_name("VG")
+
     within(".visit_groups_for_#{@arm.id}") do
       expect(page.all(".visit_name")[0].value).to eq(@original_visit_group_1.name)
       expect(page.all(".visit_name")[1].value).to eq(@new_visit_group.name)
