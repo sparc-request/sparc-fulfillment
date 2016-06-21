0b3a19255f5200ea9d8120e211ea88f6238ee5d4
Andrew Cates <amcates@gmail.com>
2016-06-21 16:14:00 -0400

    small refactor

--- lib/identity_organizations.rb
+++ lib/identity_organizations.rb
@@ -4,11 +4,20 @@ class IdentityOrganizations
   end
 
   def fulfillment_access_protocols
-    super_user_orgs = Organization.includes(:super_users).where("super_users.identity_id = ?", @id).references(:super_users).uniq(:organizations)
-    clinical_provider_orgs = Organization.includes(:clinical_providers).where("clinical_providers.identity_id = ?", @id).references(:clinical_providers).uniq(:organizations)
+    super_user_orgs = Organization.includes(:super_users).where(super_users: {identity_id: @id}).references(:super_users).uniq(:organizations)
+    clinical_provider_orgs = Organization.includes(:clinical_providers).where(clinical_providers: {identity_id: @id}).references(:clinical_providers).uniq(:organizations)
     Protocol.joins(:sub_service_request).where(sub_service_requests: {organization_id: super_user_orgs + authorized_child_organizations(super_user_orgs) + clinical_provider_orgs}).distinct
   end
 
+  def fulfillment_organizations_with_protocols
+    super_user_orgs = Organization.includes(:super_users).where(super_users: {identity_id: @id}).references(:super_users).uniq(:organizations)
+    clinical_provider_orgs = Organization.includes(:clinical_providers).where(clinical_providers: {identity_id: @id}).references(:clinical_providers).uniq(:organizations)
+
+    Organization.joins(:protocols).where(id: super_user_orgs + authorized_child_organizations(super_user_orgs) + clinical_provider_orgs)
+  end
+
+  private
+  
   def authorized_child_organizations(org_ids)
     org_ids = org_ids.flatten.compact
     if org_ids.empty?
@@ -19,10 +28,4 @@ class IdentityOrganizations
     end
   end
 
-  def fulfillment_organizations_with_protocols
-    super_user_orgs = Organization.includes(:super_users).where("super_users.identity_id = ?", @id).references(:super_users).uniq(:organizations)
-    clinical_provider_orgs = Organization.includes(:clinical_providers).where("clinical_providers.identity_id = ?", @id).references(:clinical_providers).uniq(:organizations)
-
-    Organization.joins(:protocols).where(id: super_user_orgs + authorized_child_organizations(super_user_orgs) + clinical_provider_orgs)
-  end
 end
