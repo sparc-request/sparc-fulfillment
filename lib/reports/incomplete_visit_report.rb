71097f50535210ad6fd1bf896921056d84de887f
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2016-09-13 17:56:28 -0400

    Added a column for end date. [#129558171]

--- lib/reports/incomplete_visit_report.rb
+++ lib/reports/incomplete_visit_report.rb
@@ -6,6 +6,7 @@ class IncompleteVisitReport < Report
 
   # db columns of interest; qualified because of ambiguities
   START_DATE  = '`appointments`.`start_date`'
+  END_DATE    = '`appointments`.`end_date`'
   STATUS      = '`procedures`.`status`'
   PROTOCOL_ID = '`participants`.`protocol_id`'
   LAST_NAME   = '`participants`.`last_name`'
@@ -13,7 +14,7 @@ class IncompleteVisitReport < Report
   VISIT_NAME  = :name
 
   # report columns
-  REPORT_COLUMNS = ["Protocol ID (SRID)", "Patient Last Name", "Patient First Name", "Visit Name", "Start Date", "List of Cores which have incomplete visits"]
+  REPORT_COLUMNS = ["Protocol ID (SRID)", "Patient Last Name", "Patient First Name", "Visit Name", "Start Date", "End Date", "List of Cores which have incomplete visits"]
 
   def generate(document)
     document.update_attributes(content_type: 'text/csv', original_filename: "#{@params[:title]}.csv")
@@ -25,7 +26,7 @@ class IncompleteVisitReport < Report
       result_set = Appointment.all.joins(:procedures).joins(:participant).joins(:visit_group).
                    where("#{START_DATE} < ? AND #{STATUS} = ?", _24_hours_ago, "unstarted").
                    uniq.
-                   pluck(PROTOCOL_ID, LAST_NAME, FIRST_NAME, VISIT_NAME, :start_date, :sparc_core_name)
+                   pluck(PROTOCOL_ID, LAST_NAME, FIRST_NAME, VISIT_NAME, :start_date, :end_date, :sparc_core_name)
 
       get_protocol_srids(result_set)
 
