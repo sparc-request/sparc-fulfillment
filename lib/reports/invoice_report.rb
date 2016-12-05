d6ae0f2246bab6ba6ec6171ec9733f77aa51d6d5
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2016-12-05 10:34:04 -0500

    Took out binding.pry [#131308161]

--- lib/reports/invoice_report.rb
+++ lib/reports/invoice_report.rb
@@ -110,7 +110,6 @@ class InvoiceReport < Report
 
             total += fulfillment.total_cost
             total_with_subsidy += protocol.sub_service_request.subsidy ? fulfillment.total_cost * (1 - protocol.sub_service_request.subsidy.percent_subsidy) : fulfillment.total_cost
-            binding.pry if total_with_subsidy < 0
           end
         end
 
