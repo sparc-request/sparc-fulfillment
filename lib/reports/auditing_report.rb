2901442f8d8175f17ccd51225f6b8666721359f3
Stuart Johnson <work.stuart.johnson@gmail.com>
2016-12-12 12:58:17 -0500

    finishing up part I, doing conditional title change, etc [#133960589]

--- lib/reports/auditing_report.rb
+++ lib/reports/auditing_report.rb
@@ -19,7 +19,7 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.~
 
 class AuditingReport < Report
-  VALIDATES_PRESENCE_OF = [:title, :start_date, :end_date, :service_type].freeze
+  VALIDATES_PRESENCE_OF = [:service_type, :title, :start_date, :end_date].freeze
   VALIDATES_NUMERICALITY_OF = [].freeze
 
   require 'csv'
@@ -35,58 +35,62 @@ class AuditingReport < Report
     document.update_attributes(content_type: 'text/csv', original_filename: "#{@params[:title]}.csv")
 
     CSV.open(document.path, "wb") do |csv|
-      csv << ["From", format_date(Time.strptime(@params[:start_date], "%m/%d/%Y")), "To", format_date(Time.strptime(@params[:end_date], "%m/%d/%Y"))]
-      csv << [""]
-      csv << [""]
-      csv << [
-        "Protocol ID",
-        "Patient Name",
-        "Patient ID",
-        "Arm Name",
-        "Visit Name",
-        "Service Completion Date",
-        "Marked as Incomplete Date",
-        "Marked with Follow-Up Date",
-        "Added?",
-        "Nexus Core",
-        "Service Name",
-        "Completed?",
-        "Billing Type (R/T/O)",
-        "If not completed,
-        reason and comment",
-        "Follow-Up date and comment",
-        "Cost"
-      ]
+      if @params[:service_type] == "Per Patient Per Visit"
+        csv << ["From", format_date(Time.strptime(@params[:start_date], "%m/%d/%Y")), "To", format_date(Time.strptime(@params[:end_date], "%m/%d/%Y"))]
+        csv << [""]
+        csv << [""]
+        csv << [
+          "Protocol ID",
+          "Patient Name",
+          "Patient ID",
+          "Arm Name",
+          "Visit Name",
+          "Service Completion Date",
+          "Marked as Incomplete Date",
+          "Marked with Follow-Up Date",
+          "Added?",
+          "Nexus Core",
+          "Service Name",
+          "Completed?",
+          "Billing Type (R/T/O)",
+          "If not completed,
+          reason and comment",
+          "Follow-Up date and comment",
+          "Cost"
+        ]
 
-      if @params[:protocol_ids].present?
-        protocols = Protocol.find(@params[:protocol_ids])
-      else
-        protocols = Identity.find(@params[:identity_id]).protocols
-      end
-
-      protocols.each do |protocol|
-        protocol.procedures.to_a.select { |procedure| procedure.handled_date && (@start_date..@end_date).cover?(procedure.handled_date) }.each do |procedure|
-          participant = procedure.appointment.participant
-
-          csv << [
-            protocol.sparc_id,
-            participant.full_name,
-            participant.label,
-            procedure.appointment.arm.name,
-            procedure.appointment.name,
-            format_date(procedure.completed_date.nil? ? nil : procedure.completed_date),
-            format_date(procedure.incompleted_date.nil? ? nil : procedure.incompleted_date),
-            format_date(procedure.follow_up? ? procedure.handled_date : nil),
-            added_formatter(procedure),
-            procedure.service.organization.name,
-            procedure.service_name,
-            complete_formatter(procedure),
-            procedure.formatted_billing_type,
-            reason_formatter(procedure),
-            follow_up_formatter(procedure),
-            display_cost(procedure.service_cost)
-          ]
+        if @params[:protocol_ids].present?
+          protocols = Protocol.find(@params[:protocol_ids])
+        else
+          protocols = Identity.find(@params[:identity_id]).protocols
         end
+
+        protocols.each do |protocol|
+          protocol.procedures.to_a.select { |procedure| procedure.handled_date && (@start_date..@end_date).cover?(procedure.handled_date) }.each do |procedure|
+            participant = procedure.appointment.participant
+
+            csv << [
+              protocol.sparc_id,
+              participant.full_name,
+              participant.label,
+              procedure.appointment.arm.name,
+              procedure.appointment.name,
+              format_date(procedure.completed_date.nil? ? nil : procedure.completed_date),
+              format_date(procedure.incompleted_date.nil? ? nil : procedure.incompleted_date),
+              format_date(procedure.follow_up? ? procedure.handled_date : nil),
+              added_formatter(procedure),
+              procedure.service.organization.name,
+              procedure.service_name,
+              complete_formatter(procedure),
+              procedure.formatted_billing_type,
+              reason_formatter(procedure),
+              follow_up_formatter(procedure),
+              display_cost(procedure.service_cost)
+            ]
+          end
+        end
+      elsif @params[:service_type] == "One Time Fees"
+        ##Part II Goes Here
       end
     end
   end
