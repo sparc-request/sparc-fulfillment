ecdd1474685060f63ae7cd6129e5ea8e66ded44c
Mark Lohr <markuslohr@gmail.com>
2016-12-13 09:22:48 -0500

    removed unecessary comments [#133750025]

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -30,10 +30,6 @@ class ProtocolsController < ApplicationController
       format.html { render }
       format.json do
         @protocols = current_identity.protocols
-        # @protocols = current_identity.protocols.select do |p|
-        #   p.arms.pluck(:visit_count).sum == 1
-        # end
-
         if params[:status].present? && params[:status] != 'all'
           @protocols = @protocols.select { |protocol| protocol.status == params[:status] }
         end
