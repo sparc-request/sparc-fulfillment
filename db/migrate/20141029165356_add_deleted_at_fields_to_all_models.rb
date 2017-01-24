63d6403016819392dd4cd4e82d965c1f65ca870c
Jason Leonard <jleonardw9@gmail.com>
2017-01-24 15:57:34 -0500

    Missed a migration

--- db/migrate/20141029165356_add_deleted_at_fields_to_all_models.rb
+++ db/migrate/20141029165356_add_deleted_at_fields_to_all_models.rb
@@ -23,7 +23,6 @@ class AddDeletedAtFieldsToAllModels < ActiveRecord::Migration
     add_column :protocols, :deleted_at, :datetime
     add_column :arms, :deleted_at, :datetime
     add_column :visit_groups, :deleted_at, :datetime
-    add_column :services, :deleted_at, :datetime
     add_column :line_items, :deleted_at, :datetime
     add_column :visits, :deleted_at, :datetime
   end
