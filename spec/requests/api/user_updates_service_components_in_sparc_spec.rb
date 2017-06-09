5a724379980bf40252f657fa100153449dbad189
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-06-09 08:33:07 -0400

    Message

--- spec/requests/api/user_updates_service_components_in_sparc_spec.rb
+++ spec/requests/api/user_updates_service_components_in_sparc_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe 'User updates Service Components in SPARC', type: :request, enque
     it 'should update the Protocol', sparc_api: :get_service_components_1 do
       line_item = create(:line_item, sparc_id: 1, service: create(:service_with_one_time_fee), protocol: create(:protocol), quantity_requested: 500, quantity_type: 'each')
       user_updates_service_components_in_sparc
-
+      binding.pry
       expect(line_item.components.map(&:component)).to eq ['a','b','c','o']
     end
   end
