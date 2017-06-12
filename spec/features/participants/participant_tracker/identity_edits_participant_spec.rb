adafd2f9126a4d030598298a1ac53620a735a3b7
Stuart Johnson <work.stuart.johnson@gmail.com>
2017-06-12 13:55:49 -0400

    merging participant list with participant tracker [#146186631]

--- spec/features/participants/participant_tracker/identity_edits_participant_spec.rb
+++ spec/features/participants/participant_tracker/identity_edits_participant_spec.rb
@@ -20,60 +20,38 @@
 
 require 'rails_helper'
 
-feature 'User creates Participant', js: true do
+feature 'User edits Participant', js: true do
 
-  scenario 'and sees the new Participants in the list' do
-    given_i_am_viewing_the_participant_list
-    when_i_create_a_new_participant
-    then_i_should_see_the_new_participant_in_the_list
+  scenario 'and sees the updated Participant' do
+    given_i_am_viewing_the_participant_tracker
+    when_i_update_a_participants_details
+    then_i_should_see_the_updated_details
   end
 
   def given_i_am_viewing_the_participant_list
-    @protocol = create_and_assign_protocol_to_me
+    protocol = create_and_assign_protocol_to_me
 
-    visit protocol_path(@protocol.id)
+    visit protocol_path(protocol.id)
     wait_for_ajax
 
-    click_link 'Participant List'
+    click_link 'Participant Tracker'
     wait_for_ajax
   end
 
-  def when_i_create_a_new_participant
-    find('.new-participant').click
+  def when_i_update_a_participants_details
+    page.find('table.participants tbody tr:first-child td.edit a').click
 
-    participant = build(:participant_with_protocol)
-
-    fill_in 'First Name', with: participant.first_name
-    fill_in 'Last Name', with: participant.last_name
-    fill_in 'MRN', with: participant.mrn
-    fill_in 'City', with: participant.city
-    bootstrap_select '#participant_state', participant.state
-    fill_in 'Zip Code', with: participant.zipcode
-    bootstrap_select '#participant_status', participant.status
-    page.execute_script %Q{ $("#dob_time_picker").trigger("focus") }
-
-    page.execute_script %Q{ $("td.year:contains('0')").trigger("click") }
-    page.execute_script %Q{ $("td.month:contains('Mar')").trigger("click") }
+    fill_in 'First Name', with: 'Starlord'
+    page.execute_script %Q{ $('#dob_time_picker').trigger("focus") }
     page.execute_script %Q{ $("td.day:contains('15')").trigger("click") }
-    bootstrap_select '#participant_gender', "Female"
-    bootstrap_select '#participant_ethnicity', "Hispanic or Latino"
-    bootstrap_select '#participant_race', "Asian"
-    fill_in 'Address', with: "123 Fake Street"
 
     find("input[value='Save Participant']").click
-    wait_for_ajax
+
+    refresh_bootstrap_table 'table.participants'
   end
 
-  def then_i_should_see_the_new_participant_in_the_list
-    expect(page).to have_css('#flashes_container', text: 'Participant Created')
-    expect(page).to have_css('table.participants tbody tr', count: 4)
-  end
-
-  def then_they_should_have_an_assigned_arm
-    expect(Participant.last.arm_id).to eq(@protocol.arms.first.id)
-  end
-
-  def then_they_should_not_have_an_assigned_arm
-    expect(Participant.last.arm_id).to eq(nil)
+  def then_i_should_see_the_updated_details
+    expect(page).to have_css('#flashes_container', text: 'Participant Updated')
+    expect(page).to have_css('table.participants tbody tr td.first_name', text: 'Starlord')
   end
 end
