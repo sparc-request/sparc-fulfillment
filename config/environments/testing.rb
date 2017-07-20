219006cb1a8a49edd44a650892d958c31df20172
Mark Lohr <markuslohr@gmail.com>
2017-07-20 10:45:59 -0400

    added environment config options for mailer

--- config/environments/testing.rb
+++ config/environments/testing.rb
@@ -54,4 +54,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { :host => 'cwf-d.obis.musc.edu' }
 end
