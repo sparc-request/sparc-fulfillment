69604e5f15446f14be9e33b49acdaca29342deaf
Mark Lohr <markuslohr@gmail.com>
2017-09-18 11:24:52 -0400

    moved email from yml to env

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -8,7 +8,8 @@ class TaskMailer < ApplicationMailer
   def task_confirmation(identity, task)
     @identity = identity
     @task = task
-    env = ENV.fetch('environment')
-    mail to: env == 'testing' ? {t(:task)[:test_email]} : identity.email, subject: "(SPARCFulfillment) New Task Assigned"
+    env = ENV.fetch('ENVIRONMENT')
+    test_email = ENV.fetch('TESTING_EMAIL')
+    mail to: env == 'testing' ? test_email : identity.email, subject: "(SPARCFulfillment) New Task Assigned"
   end
 end
