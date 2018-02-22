9c80f35b5d3fe7920c3cb613228cb7c2af68119f
Stuart Johnson <work.stuart.johnson@gmail.com>
2018-02-22 11:53:53 -0500

    quick fix for migration [#155263353]

--- db/migrate/20180219185258_remove_arm_id_data_from_otf_line_items.rb
+++ db/migrate/20180219185258_remove_arm_id_data_from_otf_line_items.rb
@@ -1,6 +1,6 @@
 class RemoveArmIdDataFromOtfLineItems < ActiveRecord::Migration[5.0]
   def change
-    LineItem.includes(:service).where(services: {one_time_fee: true}).where.not(arm_id: nil).each do |line_item|
+    LineItem.with_deleted.includes(:service).where(services: {one_time_fee: true}).where.not(arm_id: nil).each do |line_item|
       line_item.arm_id = nil
       line_item.save(validate: false)
     end
