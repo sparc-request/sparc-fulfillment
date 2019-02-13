bdf5a7d80b4572eb1471b42d6d0fe92771a8ba89
Mark Lohr <markuslohr@gmail.com>
2019-02-13 14:28:10 -0500

    changed tooltip [#163909683]

--- app/helpers/protocol_helper.rb
+++ app/helpers/protocol_helper.rb
@@ -21,7 +21,7 @@
 module ProtocolHelper
 
   def admin_portal_link(protocol)
-    content_tag(:a, href: protocol.sparc_uri, target: :blank, class: 'btn btn-default btn-xs admin_portal_link', title: 'Admin Portal link', aria: { expanded: 'false' }) do
+    content_tag(:a, href: protocol.sparc_uri, target: :blank, class: 'btn btn-default btn-xs admin_portal_link', title: 'Back to SPARCDashboard', aria: { expanded: 'false' }) do
       content_tag(:span, '', class: 'glyphicon glyphicon-link')
     end
   end
