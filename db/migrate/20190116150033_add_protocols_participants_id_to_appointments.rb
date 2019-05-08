84304af3f262ad1e1aaf000281dd8014b650ed30
Stuart Johnson <work.stuart.johnson@gmail.com>
2019-05-08 11:58:44 -0400

    fix for column addition

--- db/migrate/20190116150033_add_protocols_participants_id_to_appointments.rb
+++ db/migrate/20190116150033_add_protocols_participants_id_to_appointments.rb
@@ -1,6 +1,7 @@
 class AddProtocolsParticipantsIdToAppointments < ActiveRecord::Migration[5.2]
   def up
     add_column :appointments, :protocols_participant_id, :integer
+    Appointment.reset_column_information
     bad_data = []
     progress_bar = ProgressBar.new(Appointment.count)
     Appointment.find_each do |appt|
