f3f2b62616ff354923483e3c8e6f7b79c9733de4
Stuart Johnson <work.stuart.johnson@gmail.com>
2021-07-12 10:42:13 -0400

    updating staging deploy file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -53,10 +53,10 @@ server 'cwf-s.obis.musc.edu', user: 'capistrano', roles: %w{app db web}
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
 
+set :deploy_to, "/var/www/rails/sparc-fulfillment"
 set :branch, 'staging'
 set :rails_env, 'production'
-set :rvm_ruby_version, '2.4.2@fulfillment-staging --create'
-
+set :rvm_ruby_version, '2.5.5@cwf --create'
 set :passenger_restart_with_touch, true
 #set :passenger_environment_variables, { :path => '/usr/local/rvm/gems/ruby-1.9.3-p286/bin:$PATH' }
 #set :passenger_rvm_ruby_version, 'ruby-1.9.3-p286'
