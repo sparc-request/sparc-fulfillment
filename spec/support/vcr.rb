073291e64aedb30618ee79e20302d89a88acca2e
Stuart Johnson <work.stuart.johnson@gmail.com>
2021-10-07 18:00:42 -0400

    syntax fix

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -23,7 +23,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'vcr_cassettes'
   config.hook_into :webmock
   config.ignore_localhost = true
-  config.ignore_hosts ["github.com", "githubusercontent.com"]
+  config.ignore_hosts 'github.com', 'githubusercontent.com'
   config.configure_rspec_metadata!
   config.default_cassette_options = { allow_playback_repeats: true }
 end
