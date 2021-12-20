b6d4751ea068264b2a2a8ff8dc1d7a4c5381f146
royyesud@musc.edu <royyesud@musc.edu>
2021-12-20 09:40:52 -0800

    appointments/appointment_statuses folder

--- spec/features/appointments/appointment_statuses/removing_a_status_spec.rb
+++ spec/features/appointments/appointment_statuses/removing_a_status_spec.rb
@@ -39,15 +39,16 @@ feature 'Removing a Status', js: true do
     visit calendar_protocol_participant_path(id: protocols_participant.id, protocol_id: protocol)
     wait_for_ajax
 
-    bootstrap_select '#appointment_select', visit_group.name
+    find("#appointmentsList a[data-appointment-id='#{visit_group.id}']").click
     wait_for_ajax
   end
 
   def when_i_deselect_an_appointment_status
     # For fun of course
-    bootstrap_select '#appointment_indications', "Skipped Visit"
+    bootstrap_select '#statuses', "Skipped Visit"
     find('body').click()
-    bootstrap_select '#appointment_indications', "Skipped Visit"
+    bootstrap_select '#statuses', "Skipped Visit"
+    wait_for_ajax
   end
 
   def then_the_appointment_status_should_be_destroyed_for_that_appointment
