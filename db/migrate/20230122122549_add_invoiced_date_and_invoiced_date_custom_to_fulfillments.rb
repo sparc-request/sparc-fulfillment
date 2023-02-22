76d04ef8699febfb245c6b5ec6b5430d457d9074
Greg Mathias <mathiasgreg@gmail.com>
2023-02-22 11:40:49 -0500

    Remove migration and restore model

--- db/migrate/20230122122549_add_invoiced_date_and_invoiced_date_custom_to_fulfillments.rb
+++
@@ -1,6 +0,0 @@
-class AddInvoicedDateAndInvoicedDateCustomToFulfillments < ActiveRecord::Migration[5.2]
-  def change
-    add_column :fulfillments, :invoiced_date, :datetime, after: :invoiced
-    add_column :fulfillments, :invoiced_date_custom, :datetime, after: :invoiced_date
-  end
-end
