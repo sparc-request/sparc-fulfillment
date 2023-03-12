9a3e3595abe730b7fc1273d559603d6587fbe9ce
Greg Mathias <mathiasgreg@gmail.com>
2023-03-12 09:51:55 -0400

    Remove unused target attr

--- app/helpers/procedures_helper.rb
+++ app/helpers/procedures_helper.rb
@@ -106,7 +106,7 @@ module ProceduresHelper
     if current_identity.billing_manager_protocols_allow_credit.include?(procedure.protocol)
       form_for procedure, url: appointment_procedure_path(procedure, appointment_id: procedure.appointment_id), remote: true, method: :put do |f|
         content_tag :div, class: 'tooltip-wrapper', title: tooltip, data: { toggle: 'tooltip' } do
-          f.check_box :credited, disabled: disabled, onchange: "Rails.fire(this.form, 'submit')", class: ['btn btn-light', disabled ? 'disabled' : ''], data: { toggle: 'toggle', id: procedure.id, on: t('constants.yes_select'), off: t('constants.no_select'), target: "#procedure#{procedure.id}Invoiced" }
+          f.check_box :credited, disabled: disabled, onchange: "Rails.fire(this.form, 'submit')", class: ['btn btn-light', disabled ? 'disabled' : ''], data: { toggle: 'toggle', id: procedure.id, on: t('constants.yes_select'), off: t('constants.no_select') }
         end
       end
     else
