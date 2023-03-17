2eaa98a460270cd09d85592bdf7648c0e23d3435
Greg Mathias <mathiasgreg@gmail.com>
2023-03-17 07:35:15 -0400

    Remove extra space

--- app/models/klok_project.rb
+++ app/models/klok_project.rb
@@ -26,7 +26,6 @@ class KlokProject < ApplicationRecord
   has_many :klok_entries, foreign_key: :project_id
   has_many :klok_people, foreign_key: :resource_id, through: :klok_entries
 
-
   delegate :local_protocol, to: :klok_entry, allow_nil: true
 
   def service
