80f24cd578959a2e7c050f088dba5352e240a888
Greg Mathias <mathiasgreg@gmail.com>
2023-03-25 14:23:16 -0400

    Fix formatting in notes column of fulfillments table

--- app/controllers/fulfillments_controller.rb
+++ app/controllers/fulfillments_controller.rb
@@ -78,7 +78,7 @@ class FulfillmentsController < ApplicationController
     respond_to :js
     persist_original_attributes_to_track_changes
     @fulfillment.update_attributes(invoiced: fulfillment_params[:invoiced])
-    @fulfillment.update_attributes(invoiced_date: fulfillment_params[:invoiced_date])
+    @fulfillment.update_attributes(invoiced_date: fulfillment_params[:invoiced_date]) if change_in_invoiced_date_detected?
     @fulfillment.update_attributes(credited: !fulfillment_params[:invoiced])
     detect_changes_and_create_notes
   end
@@ -98,6 +98,14 @@ class FulfillmentsController < ApplicationController
 
   private
 
+  def change_in_invoiced_date_detected?
+    if fulfillment_params[:invoiced_date]
+      Time.strptime(fulfillment_params[:invoiced_date], "%m/%d/%Y") != @fulfillment.invoiced_date
+    else
+      return false
+    end
+  end
+
   def persist_original_attributes_to_track_changes
     @original_attributes = @fulfillment.attributes
   end
@@ -110,10 +118,10 @@ class FulfillmentsController < ApplicationController
       unless new_field.blank?
         unless current_field.blank?
           current_field = ((field == :fulfilled_at) || (field == :invoiced_date) ? current_field.to_date.to_s : current_field.to_s)
-          new_field = ((field == :fulfilled_at) || (field == :invoiced_date) ? Time.strptime(new_field, "%m/%d/%Y").to_s : new_field.to_s)
+          new_field = ((field == :fulfilled_at) || (field == :invoiced_date) ? new_field.to_s : new_field.to_s)
         end
         if current_field != new_field
-          comment = t(:fulfillment)[:log_notes][field] + (field == :performer_id ? Identity.find(new_field).full_name : new_field.to_s)
+          comment = t(:fulfillment)[:log_notes][field] + (field == :performer_id ? Identity.find(new_field).full_name : (field == :fulfilled_at || field == :invoiced_date) ? new_field.to_s : new_field.to_s)
           @fulfillment.notes.create(kind: 'log', comment: comment, identity: current_identity)
         end
       end
