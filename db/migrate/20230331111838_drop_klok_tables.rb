20fbfeaa5995a3359d84242ff0be15afea717cce
Greg Mathias <mathiasgreg@gmail.com>
2023-04-06 12:44:18 -0400

    Only drop_table if exists and enabled default 0

--- db/migrate/20230331111838_drop_klok_tables.rb
+++ db/migrate/20230331111838_drop_klok_tables.rb
@@ -1,8 +1,8 @@
 class DropKlokTables < ActiveRecord::Migration[5.2]
   def up
-    drop_table :klok_entries
-    drop_table :klok_people
-    drop_table :klok_projects
+    drop_table :klok_entries, if_exists: true
+    drop_table :klok_people, if_exists: true
+    drop_table :klok_projects, if_exists: true
   end
 
   def down
