f079d525bce2ffa0186a2f27444dc6e15322720b
Greg Mathias <mathiasgreg@gmail.com>
2023-04-06 12:51:37 -0400

    visible: false, expect pass

--- spec/support/features/visit_helpers.rb
+++ spec/support/features/visit_helpers.rb
@@ -37,7 +37,7 @@ module Features
       visit calendar_protocol_participant_path(id: @protocols_participant.id, protocol_id: @protocol)
       wait_for_ajax
 
-      first('a.list-group-item.appointment-link').click
+      first('a.list-group-item.appointment-link', visible: false).click
       wait_for_ajax
     end
 
