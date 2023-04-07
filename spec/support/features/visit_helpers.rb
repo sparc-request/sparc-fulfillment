10d1b79a3707773c218ed49617af0a9de548976e
Greg Mathias <mathiasgreg@gmail.com>
2023-04-07 12:03:40 -0400

    Accept incoming

--- spec/support/features/visit_helpers.rb
+++ spec/support/features/visit_helpers.rb
@@ -36,12 +36,8 @@ module Features
     def given_i_am_viewing_a_visit
       visit calendar_protocol_participant_path(id: @protocols_participant.id, protocol_id: @protocol)
       wait_for_ajax
-<<<<<<< ours
-      
-=======
 
       using_wait_time = 15
->>>>>>> theirs
       first('a.list-group-item.appointment-link').click
       wait_for_ajax
     end
@@ -52,7 +48,7 @@ module Features
 
       first('a.list-group-item.appointment-link').click
       wait_for_ajax
-      
+
       find('a.btn.start-appointment').click
       wait_for_ajax
     end
