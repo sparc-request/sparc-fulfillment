6e1876e4eb452a0b694eb92c5b831b76f8fcddfe
Ito <ito.eta1@gmail.com>
2023-05-02 15:10:45 -0400

    added changes to tests to reflect work done on this branch

--- config/environments/testing.rb
+++ config/environments/testing.rb
@@ -55,4 +55,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { :host => 'cwf-d.obis.musc.edu' }
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
 end
