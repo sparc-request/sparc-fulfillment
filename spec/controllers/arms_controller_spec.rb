a04dba4be17f86f4ea955ae38a353e03b0c411bb
Greg Mathias <mathiasgreg@gmail.com>
2024-01-07 14:25:31 -0500

    Remove comments

--- spec/controllers/arms_controller_spec.rb
+++ spec/controllers/arms_controller_spec.rb
@@ -26,10 +26,6 @@ RSpec.describe ArmsController do
     sign_in
     @protocol = create(:protocol)
     @arm = create(:arm, protocol_id: @protocol.id)
-    #@clinical_services = create_list(:service, 3, one_time_fee: false)
-    #@clinical_services.each do |service|
-    #  create(:line_item, service: service, protocol: @protocol)
-    #end
   end
 
   describe "GET #new" do
