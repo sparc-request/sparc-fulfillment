ac7aceeb3bf800580d767a67446177f639c063a1
Greg Mathias <mathiasgreg@gmail.com>
2024-03-17 10:54:47 -0400

    ci

--- spec/features/appointments/appointment_calendar/grouped_services/identity_changes_a_procedure_spec.rb
+++ spec/features/appointments/appointment_calendar/grouped_services/identity_changes_a_procedure_spec.rb
@@ -122,7 +122,8 @@ feature 'Identity changes a Service', js: true do
 
   def when_i_move_all_procedures_out_of_the_group
     wait_for_ajax
-    find('tr.info.groupBy.expanded', visible: :all).click
+    # find('tr.info.groupBy.expanded', visible: :all).click
+    find('tr[data-group-index="1"]', visible: :all).click
     @original_group_id = page.first('tr td.name div')['data-group-id']
     bootstrap_select '#procedure_billing_type', 'R'
     wait_for_ajax
