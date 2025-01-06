feaa3382adf238e36533c7dd2ee9a006d13f1f1a
Greg Mathias <mathiasgreg@gmail.com>
2025-01-06 16:07:24 -0500

    Keep leading zero in period column by adding an apostrophe

--- app/models/concerns/reports_shared_methods.rb
+++ app/models/concerns/reports_shared_methods.rb
@@ -44,7 +44,7 @@ module ReportsSharedMethods
   def fiscal_year_month_display(completed_date, fiscal_start_month = 7)
     month_number = ((completed_date.month - fiscal_start_month) % 12) + 1
     abbreviated_month = completed_date.strftime("%b")
-    sprintf("%02d-%s", month_number, abbreviated_month)
+    sprintf("'%02d-%s", month_number, abbreviated_month)
   end
 
   def fiscal_year_display(completed_date, fiscal_start_month = 7)
