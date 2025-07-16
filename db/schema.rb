e2da81af310b3deae66ac017a5eed56e8ee90081
jbvanmeter <james.blair.vanmeter@gmail.com>
2025-07-15 19:09:01 -0500

    Paperclip attachments converted to Active Storage attachments (active_storage_attachments and active_storage_blobs tables populated with relevant data)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_27_015456) do
+ActiveRecord::Schema.define(version: 2025_07_10_162611) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
